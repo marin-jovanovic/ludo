d767787040118a6f7d54386d4c576f8f8f5d1d4a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-12 13:51:28 +0200

    src_py git.sh - impl: begin project

---
+++ src_py/git.sh
@@ -0,0 +1,18 @@
+git add /home/tea/Documents/git/marin-jovanovic/ludo/frontend
+git commit -m "frontend  - impl: begin project"
+git add /home/tea/Documents/git/marin-jovanovic/ludo/node_modules
+git commit -m "node_modules  - impl: begin project"
+git add /home/tea/Documents/git/marin-jovanovic/ludo/src_py/auto_git.py
+git commit -m "src_py auto_git.py - impl: begin project"
+git add /home/tea/Documents/git/marin-jovanovic/ludo/api
+git commit -m "api  - impl: begin project"
+git add /home/tea/Documents/git/marin-jovanovic/ludo/package.json
+git commit -m "package.json - impl: begin project"
+git add /home/tea/Documents/git/marin-jovanovic/ludo/package-lock.json
+git commit -m "package-lock.json - impl: begin project"
+git add /home/tea/Documents/git/marin-jovanovic/ludo/.gitignore
+git commit -m ".gitignore - impl: begin project"
+git add /home/tea/Documents/git/marin-jovanovic/ludo/src_py/git.sh
+git commit -m "src_py git.sh - impl: begin project"
+git add /home/tea/Documents/git/marin-jovanovic/ludo/src_py/main.py
+git commit -m "src_py main.py - impl: begin project"
