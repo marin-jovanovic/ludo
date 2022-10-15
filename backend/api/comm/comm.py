30af9ee9054e6c9055e1317f37df2a71db851143
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-15 23:26:11 +0200

    backend api comm comm.py - impl: create user in db

--- backend/api/comm/comm.py
+++ backend/api/comm/comm.py
@@ -1,5 +1,23 @@
 import json
 
+import argparse
+import base64
+import getpass
+import hashlib
+import json
+import sys
+import time
+from os import path
+from bcrypt import hashpw, gensalt
+
+
+def sleep_random():
+    salt = gensalt()
+    for i in reversed(salt.decode("utf-8")):
+        t = int(str(ord(i))[-1])
+        if t in [1, 2, 3]:
+            time.sleep(t)
+            break
 
 def pretty_print_json(payload):
     print(json.dumps(payload, indent=4, sort_keys=True))
