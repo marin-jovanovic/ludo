9e774c1f44dc750ca003579f41e425afa01413c6
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-15 20:30:19 +0200

    backend api startup.py - ref: repo structure

---
+++ backend/api/startup.py
@@ -0,0 +1,22 @@
+from backend.api.assets.printer import print_logo
+from backend.api.role_action_validation.role_validator import SchemeValidator
+
+class StartupConfig:
+
+    def __init__(self):
+        self.scheme_validator = None
+
+    def init_scheme_validator(self, config):
+        self.scheme_validator = SchemeValidator(config)
+
+    def get_scheme_validator(self):
+        if not self.scheme_validator:
+            print("scheme_validator is not initialized")
+
+        return self.scheme_validator
+
+    @staticmethod
+    def print_app_logo():
+        print_logo()
+
+startup_configuration = StartupConfig()
