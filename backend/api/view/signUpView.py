36c3726197624b63751e3baa3fb0be8e6e02e024
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-15 23:26:11 +0200

    backend api view signUpView.py - impl: create user in db

--- backend/api/view/signUpView.py
+++ backend/api/view/signUpView.py
@@ -2,18 +2,17 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 
 from backend.api.view.comm import get_auth_ok_response_template
-from backend.api.auth.main import create_user
-
+# from backend.api.auth.main import create_user
+from backend.api.cqrs_c.users import create_user
 class SignUpView(APIView):
 
     def post(self, request, username):
 
-        print("signup post", username)
+        # print("signup post", username)
 
         password = request.headers['password']
-        print(f"{password=}")
+        # print(f"{password=}")
 
-        create_user(username, password)
 
 
         # todo add to db
@@ -22,6 +21,6 @@ class SignUpView(APIView):
         # return tokens
 
         response = get_auth_ok_response_template(request)
-        response["payload"]["status"] = True
+        response["payload"]["status"] = create_user(username, password)
 
         return JsonResponse(response)
