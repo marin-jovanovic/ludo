8006102188a7a4f67d94cd18ede4a9f973385dd2
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-15 20:43:54 +0200

    backend api assets  - ref: repo structure

---
+++ backend/api/comm/scheme_validator.py
@@ -0,0 +1,73 @@
+
+class SchemeValidator:
+    """check if role can perform given action"""
+
+    def __init__(self, role_validation_cfg):
+        self.scheme = role_validation_cfg
+        self.serialization_connector = ";"
+
+    # pass url and action, remove url from action
+    def check_action(self, role, action):
+        # print(f"check action {role=} {action=}")
+        if isinstance(action, str):
+            action = self.deserialize(action)
+
+        if len(action) == 1:
+            return False
+
+        if isinstance(role, list):
+            if len(role) != 1:
+                return any([self.check_action(r, action) for r in role])
+
+            role = role[0]
+
+        if role not in self.scheme["roles"]:
+            return False
+
+        t = self.scheme["roles"][role]
+        table = action[0]
+        action = action[1]
+        if table not in t:
+            return False
+        t = t[table]
+        if action not in t:
+            return False
+        return t[action]
+
+    def get_all_tables(self):
+        r = []
+        for role, tables in self.scheme["roles"].items():
+
+            for t, actions in tables.items():
+                r.append(t)
+
+        return r
+
+    # todo remove
+    def deserialize(self, payload):
+
+        splitters = [self.serialization_connector, " ", ":"]
+
+        if not any([payload.__contains__(i) for i in splitters]):
+            """assumption: already deserialized"""
+            return [payload]
+
+        for j in splitters:
+            if payload.__contains__(j):
+                return [i.strip() for i in
+                        payload.split(j, 1)]
+
+
+#
+def main():
+    from backend.api.comm.json_loader import role_validation_cfg
+
+    scheme_validator = SchemeValidator(role_validation_cfg)
+
+
+
+    print("test ok")
+
+
+if __name__ == '__main__':
+    main()
---
+++ backend/api/middleware/jumper_middleware.py
@@ -0,0 +1,24 @@
+from decouple import config
+
+from backend.api.cqrs_q.csrf import get_by_ip
+from backend.api.comm.comm import bytes_to_json
+
+class JumperMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+        self.debug = config("DEBUG") != "0"
+
+    def __call__(self, request):
+        print()
+        # print("auto middleware")
+
+        # fixme only for testing
+        request.access_token = "tmp"
+        request.refresh_token = "tmp"
+        request.username = "a"
+        request.ip = "127.0.0.1"
+        request.synchronizer_token_match = True
+        request.role = "role 1"
+
+        return self.get_response(request)
