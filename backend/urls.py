f09f284753aae0d220cf239846bc952ad1a84570
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-15 20:43:54 +0200

    backend urls.py - ref: repo structure

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import routers
 
-from backend.api.view.loginView import LoginView
+# from backend.api.view.loginView import LoginView
 from backend.api.view.signUpView import SignUpView
 
 from backend.api.startup import startup_configuration
@@ -11,6 +11,6 @@ api_router = routers.DefaultRouter()
 
 urlpatterns = [
     path("signup/<str:username>", SignUpView.as_view()),
-    path("login/<str:username>", LoginView.as_view()),
+    # path("login/<str:username>", LoginView.as_view()),
 
 ]
