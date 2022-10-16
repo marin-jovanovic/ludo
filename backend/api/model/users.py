1c51584ec23405824361cddbe631d7b1efdbeb9c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-16 10:00:30 +0200

    backend api model users.py - impl: create user in db

--- backend/api/model/users.py
+++ backend/api/model/users.py
@@ -2,5 +2,8 @@ from django.db import models
 
 
 class Users(models.Model):
+    # alternate primary key
     username = models.TextField()
+
     password_hash = models.TextField()
+    access_token = models.TextField()
