dc5ad402090313f22eb35f7e9deb1b5955d7322c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-16 13:04:41 +0200

    test auth.py - impl: login api

--- test/auth.py
+++ test/auth.py
@@ -18,11 +18,78 @@ def create_user(u,p):
     )
 
     print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+    return json.loads(t.text)
 
+
+def auth_user(u,p):
+    url = "http://localhost:8000/login"
+
+    t = requests.post(
+        f"{url}/{u}",
+        headers= {
+            'password': p
+        },
+        data={
+            # "portfolio": portfolio
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+    return json.loads(t.text)
+
+def is_valid(u,p):
+    url = "http://localhost:8000/validate"
+
+    t = requests.post(
+        f"{url}/{u}",
+        headers= {
+            'accessToken': p
+        },
+        data={
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+    return json.loads(t.text)
+
+def logout(u,a):
+    url = "http://localhost:8000/logout"
+
+    t = requests.post(
+        f"{url}/{u}",
+        headers= {
+            'accessToken': a
+        },
+        data={
+        },
+        verify=False
+    )
+
+    print(json.dumps(json.loads(t.text), indent=4, sort_keys=True))
+    return json.loads(t.text)
 
 def main():
     print('running tests')
-    create_user('username1', 'password1')
+
+    username = 'username2'
+    password = 'password2'
+
+    create_user(username, password)
+    print(80 * "-")
+
+    # generate access token
+    access_token = auth_user(username, password)["payload"]["payload"]["access_token"]
+    print(f"{access_token=}")
+    print(80 * "-")
+
+    validated = is_valid(username, access_token)['payload']
+    print(f"{validated=}")
+    print(80 * "-")
+
+    logout(username, access_token)
+
 
 if __name__ == '__main__':
     main()
