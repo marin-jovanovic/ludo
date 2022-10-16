685be415873f56439ff1d10cb6d27be9ab85af36
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-16 13:04:41 +0200

    backend api cqrs_c users.py - impl: login api

--- backend/api/cqrs_c/users.py
+++ backend/api/cqrs_c/users.py
@@ -1,15 +1,27 @@
 from backend.api.model.users import Users
 from backend.api.auth.user_menagment import get_hashed_password, is_pass_ok
-from backend.api.cqrs_q.users import is_username_in_db, is_authenticated
-
+from backend.api.cqrs_q.users import is_username_in_db, is_authenticated, is_access_token_correct
+from backend.api.auth.main import generate_access_token
 # todo change username
 # todo access token
+
+def logout(username,access_token):
+    if not is_access_token_correct(username, access_token):
+        return {'status': False, 'debug': 'user + access token combination err'}
+
+    k, _ = Users.objects.update_or_create(
+        username=username, defaults={"access_token": None}
+    )
+    k.save()
+
+    return {'status': True, 'payload': {}}
+
 
 def auth_user(username, password):
     if not is_authenticated(username, password):
         return {'status': False, 'debug': 'user + pass combination err'}
 
-    access_token = 'ac token'
+    access_token = generate_access_token()
 
     k, _ = Users.objects.update_or_create(
         username=username, defaults={"access_token": access_token}
@@ -68,3 +80,4 @@ def delete_user(username, password):
     Users.objects.filter(username=username, password=password).delete()
 
     return {'status': True}
+
