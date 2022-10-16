ae13949ed3244a6eef5f6e962ab9bb466478266e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-16 13:04:41 +0200

    backend urls.py - impl: login api

--- backend/urls.py
+++ backend/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 from rest_framework import routers
 
+from backend.api.view.login_view import LoginView
 # from backend.api.view.loginView import LoginView
-from backend.api.view.signUpView import SignUpView
-
+from backend.api.view.signup_view import SignUpView
+from backend.api.view.logout_view import LogoutView
 from backend.api.startup import startup_configuration
 startup_configuration.print_app_logo()
-
+from backend.api.view.validation_view import ValidationView
 api_router = routers.DefaultRouter()
 
 urlpatterns = [
     path("signup/<str:username>", SignUpView.as_view()),
+    path("login/<str:username>", LoginView.as_view()),
+    path("logout/<str:username>", LogoutView.as_view()),
+    path("validate/<str:username>", ValidationView.as_view()),
+
     # path("login/<str:username>", LoginView.as_view()),
 
 ]
