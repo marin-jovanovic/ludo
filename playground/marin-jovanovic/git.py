fabe7ade0e074383bb72611f99a5f1f37a1bfae1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-17 12:26:15 +0200

    playground marin-jovanovic git.py - impl: game init

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -18,7 +18,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: game init"
+    comment = "impl: game init"
 
 
     if same_comment:
