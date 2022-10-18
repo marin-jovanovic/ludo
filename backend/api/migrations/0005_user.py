a04fec5c62ca914ead31578f5d6b7cfa57bf6cbc
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-18 13:18:03 +0200

    backend api migrations 0005_user.py - wip: ws

--- backend/api/migrations/0005_user.py
+++ backend/api/migrations/0005_user.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('api', '0004_consumptionvalues_consumptionentry'),
     ]
@@ -13,9 +12,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                                        serialize=False, verbose_name='ID')),
                 ('zoom_user_location', models.BooleanField()),
-                ('username', models.CharField(blank=True, max_length=200, null=True)),
+                ('username',
+                 models.CharField(blank=True, max_length=200, null=True)),
             ],
         ),
     ]
