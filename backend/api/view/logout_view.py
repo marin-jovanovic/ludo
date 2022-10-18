982489987d7d255dc4bc89d80b40949713905b35
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-18 13:18:03 +0200

    backend api view logout_view.py - wip: ws

--- backend/api/view/logout_view.py
+++ backend/api/view/logout_view.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from backend.api.view.comm import get_auth_ok_response_template
 from backend.api.cqrs_c.users import logout
+from backend.api.view.comm import get_auth_ok_response_template
+
 
 class LogoutView(APIView):
 
