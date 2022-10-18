61542fce5543afcfec54777adb3c98c064cce938
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-18 13:18:03 +0200

    playground marin-jovanovic git.py - wip: ws

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -18,7 +18,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: game main driver"
+    comment = "wip: ws"
 
 
     if same_comment:
