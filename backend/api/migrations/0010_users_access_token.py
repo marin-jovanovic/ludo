d8863b7957fac8cc6a5c01cdf79a973d37072ee2
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-18 13:18:03 +0200

    backend api migrations 0010_users_access_token.py - wip: ws

--- backend/api/migrations/0010_users_access_token.py
+++ backend/api/migrations/0010_users_access_token.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('api', '0009_alter_users_password_hash_alter_users_username'),
     ]
