0d55b7dcab63a8cadffd915535c6de79bbef5a9f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-18 13:18:03 +0200

    backend api comm constants.py - wip: ws

--- backend/api/comm/constants.py
+++ backend/api/comm/constants.py
@@ -1,8 +1,8 @@
 from decouple import config
 
-EXISTS= "exists"
-NOT_EXISTS="not exists"
-CREATED="created"
+EXISTS = "exists"
+NOT_EXISTS = "not exists"
+CREATED = "created"
 
 MIDDLEWARE_NO_ACTION = config("CONFIG_DEFAULT_ACTION")
 INTERNAL_SERVER_ERROR_MESSAGE = "internal server error"
