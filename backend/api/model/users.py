7ce53b3081d06ad638306685f6753e04a7ce8b80
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-19 15:42:35 +0200

    backend api model users.py - wip: game model

--- backend/api/model/users.py
+++ backend/api/model/users.py
@@ -1,4 +1,14 @@
 from django.db import models
+# from backend.api.model.game import _get_game_model
+from django.apps import apps
+
+
+
+# def _get_game_model():
+#     return apps.get_model("api.Game")
+
+def get_game_model():
+    return "api.game"
 
 
 class Users(models.Model):
@@ -7,3 +17,16 @@ class Users(models.Model):
 
     password_hash = models.TextField()
     access_token = models.TextField(null=True)
+
+    # todo check if this is desired action
+    #   if user deletes it's profile while game is in progress
+    #   then game will be deleted
+    currently_playing = models.ForeignKey(get_game_model(), null=True, on_delete=models.SET_NULL)
+
+    game_role = models.TextField(null=True)
+
+# def get_game_model():
+
+
+def get_user_model():
+    return apps.get_model("api.Users")
