d5b357119142595e2c0b1e3231626fc41950c065
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-19 21:44:06 +0200

    backend api view settings_view.py - wip: game model

---
+++ backend/api/view/settings_view.py
@@ -0,0 +1,33 @@
+
+from django.http import JsonResponse
+from rest_framework.views import APIView
+
+# from backend.api.model.user import User
+from backend.api.view.comm import get_auth_ok_response_template
+
+
+class SettingsView(APIView):
+
+    def get(self, request):
+        response = get_auth_ok_response_template(request)
+
+        # s = get_settings(username=request.username)
+
+        response["payload"] = {
+            "status": True,
+            "zoomUserLocation": "s.zoomUserLocation",
+            "username": "request.username"
+        }
+
+        return JsonResponse(response)
+
+    def post(self, request):
+        print(request.data)
+
+        # zoomUserLocation
+        # update_settings(request.username, request.data)
+
+        response = get_auth_ok_response_template(request)
+        response["payload"]["status"] = True
+        return JsonResponse(response)
+
