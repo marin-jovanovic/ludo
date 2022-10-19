9e2f9b89fbc216fbe1b2b73227a783aa3a5a814f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-19 15:42:34 +0200

    backend urls.py - wip: game model

--- backend/urls.py
+++ backend/urls.py
@@ -7,6 +7,8 @@ from backend.api.view.logout_view import LogoutView
 from backend.api.startup import startup_configuration
 from backend.api.view.validation_view import ValidationView
 
+from backend.api.view.lobby_view import LobbyView
+
 
 startup_configuration.print_app_logo()
 api_router = routers.DefaultRouter()
@@ -17,9 +19,10 @@ urlpatterns = [
     path("logout/<str:username>", LogoutView.as_view()),
     path("validate/<str:username>", ValidationView.as_view()),
 
-    path("lobby/<str:username>", ValidationView.as_view()),
+    # path("lobby/<str:username>", ValidationView.as_view()),
 
-    # path("game/")
+    path("game/", LobbyView.as_view()),
+    path("game/<str:name>", LobbyView.as_view()),
 ]
 
 
