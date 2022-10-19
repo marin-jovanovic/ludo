9f0393b104c4508f74318ce7a8612d3735d30639
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-19 15:42:34 +0200

    backend api middleware jumper_middleware.py - wip: game model

--- backend/api/middleware/jumper_middleware.py
+++ backend/api/middleware/jumper_middleware.py
@@ -1,4 +1,10 @@
 from decouple import config
+import urllib
+
+from django.http import JsonResponse
+
+from backend.api.comm.comm import get_empty_response_template
+from backend.api.cqrs_q.users import is_access_token_correct
 
 
 class JumperMiddleware:
@@ -9,12 +15,37 @@ class JumperMiddleware:
 
     def __call__(self, request):
         print()
-        # print("auto middleware")
+
+        authorization_header = request.META['HTTP_AUTHORIZATION']
+        parsed_authorization_header = urllib.parse.unquote(authorization_header)
+        auth_type, payload = parsed_authorization_header.split(" ")
+
+        username = None
+        access_token = None
+
+        if auth_type == "Digest":
+            rejection = get_empty_response_template()
+            rejection["debug"] = "not implemented"
+            return JsonResponse(rejection)
+
+        elif auth_type== "Custom":
+            username, access_token = payload.split(":")
+            print(f"{username=}")
+            print(f"{access_token=}")
+
+            r = is_access_token_correct(username, access_token)
+
+            if not r:
+
+                rejection = get_empty_response_template()
+                rejection["debug"] = "not is_validated"
+                return JsonResponse(rejection)
+
+        request.username = username
 
         # fixme only for testing
         request.access_token = "tmp"
         request.refresh_token = "tmp"
-        request.username = "a"
         request.ip = "127.0.0.1"
         request.synchronizer_token_match = True
         request.role = "role 1"
