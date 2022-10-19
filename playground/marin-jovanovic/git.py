ebc427dbcdb0c5834b161adcae84d74f40f42851
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-19 10:32:54 +0200

    playground marin-jovanovic git.py - wip: ws observer

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -18,7 +18,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: ws"
+    comment = "wip: ws observer"
 
 
     if same_comment:
