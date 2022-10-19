3bea360e849d88672a017ae67c87f8ef13e3ff59
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-19 15:42:35 +0200

    backend api cqrs_c users.py - wip: game model

--- backend/api/cqrs_c/users.py
+++ backend/api/cqrs_c/users.py
@@ -3,18 +3,19 @@ from threading import Thread
 from backend.api.auth.main import generate_access_token
 from backend.api.auth.main import get_hashed_password, is_pass_ok
 from backend.api.cqrs_q.users import is_username_in_db, is_authenticated, \
-    is_access_token_correct
-from backend.api.model.users import Users
+    is_access_token_correct, get_user
+from backend.api.model.users import get_user_model
 from backend.api.cqrs_q.users import get_logged_users
 
 # todo change username
 # todo access token
 
+
 def logout(username, access_token):
     if not is_access_token_correct(username, access_token):
         return {'status': False, 'debug': 'user + access token combination err'}
 
-    k, _ = Users.objects.update_or_create(
+    k, _ = get_user_model().objects.update_or_create(
         username=username, defaults={"access_token": None}
     )
     k.save()
@@ -34,18 +35,11 @@ class AuthNotifier:
     def notify(self, msg):
 
         """Alert the observers"""
-        print("-")
-        print(msg)
         # print(msg)
-        print(self._observers)
-
-        for i in self._observers:
-            print(i)
-            i.update("a")
 
         for observer in self._observers:
             # if modifier != observer:
-            print("send update")
+            # print("send update")
             observer.update(msg)
 
     def attach(self, observer):
@@ -65,10 +59,10 @@ class AuthNotifier:
         except ValueError:
             pass
 
-auth_notifier = AuthNotifier()
+        print(self._observers)
 
-# if __name__ == '__main__':
-#     auth_notifier.notify()
+active_users_notifier = AuthNotifier()
+
 
 def auth_user(username, password):
 
@@ -77,20 +71,13 @@ def auth_user(username, password):
 
     access_token = generate_access_token()
 
-    k, _ = Users.objects.update_or_create(
+    k, _ = get_user_model().objects.update_or_create(
         username=username, defaults={"access_token": access_token}
     )
     k.save()
 
-    print("notify")
     logged_users = get_logged_users()
-    # print(f"------ {logged_users=}")
-    # print(type(logged_users))
-
-
-
-    auth_notifier.notify("bbbbbb")
-    # auth_notifier.notify(logged_users)
+    active_users_notifier.notify(logged_users)
 
     return {'status': True, 'payload': {'access_token': access_token}}
 
@@ -99,7 +86,7 @@ def create_user(username, password):
     if is_username_in_db(username):
         return {'status': False, 'debug': 'user already in db'}
 
-    k, _ = Users.objects.update_or_create(
+    k, _ = get_user_model().objects.update_or_create(
         username=username,
         defaults={"password_hash": get_hashed_password(password)}
     )
@@ -122,7 +109,7 @@ def change_password(username, old_password, new_password_1, new_password_2):
     if not new_password_1 == new_password_2:
         return {'status': False, 'debug': 'pw1 != pw2'}
 
-    k, _ = Users.objects.update_or_create(
+    k, _ = get_user_model().objects.update_or_create(
         username=username,
         defaults={"password_hash": get_hashed_password(new_password_1)}
     )
@@ -137,6 +124,44 @@ def delete_user(username, password):
     if not is_authenticated(username, password):
         return {'status': False, 'debug': 'username + pass combo err'}
 
-    Users.objects.filter(username=username, password=password).delete()
+    get_user_model().objects.filter(username=username, password=password).delete()
 
     return {'status': True}
+
+def join_user_to_game(username, game):
+    pass
+
+def make_user_available_to_play(username):
+
+    r = get_user(username)
+    if r["status"]:
+        creator_o = r["payload"]
+    else:
+        return r
+
+    _assign_role_to_user(creator_o, None)
+
+    return {"status": True}
+
+def make_user_game_creator(username):
+
+    # fixme what if he is currently in another game?
+
+    r = get_user(username)
+    if r["status"]:
+        creator_o = r["payload"]
+    else:
+        return r
+
+    if not creator_o.game_role:
+        print("empty")
+    else:
+        return {"status" : False, "payload": "user role not empty"}
+
+    _assign_role_to_user(creator_o, "creator")
+
+    return {"status": True}
+
+def _assign_role_to_user(user_o, role):
+    user_o.game_role = role
+    user_o.save()
