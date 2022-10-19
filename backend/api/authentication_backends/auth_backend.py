df9874d538082cf9d783103f59349b14e41c8445
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-19 15:42:35 +0200

    backend api authentication_backends  - wip: game model

---
+++ backend/api/authentication_backends/auth_backend.py
@@ -0,0 +1,34 @@
+
+from django.contrib.auth.backends import BaseBackend
+
+from django.contrib.auth.models import User
+
+
+class AuthBackend(BaseBackend):
+    """
+    Authenticate against the settings ADMIN_LOGIN and ADMIN_PASSWORD.
+
+    Use the login name and a hash of the password.
+    """
+
+    def authenticate(self, request, username=None, password=None):
+        print("auth backend")
+        print(80 * "-")
+        print(f"{username=} {password=}")
+
+        username="-1"
+        password="-1"
+
+        try:
+            user = User.objects.get(username=username)
+
+        except User.DoesNotExist:
+            user = User.objects.create_user(username, username,
+                                            password)
+
+            user = User.objects.get(username=username)
+
+        user.is_active = True
+        user.save()
+
+        return user
