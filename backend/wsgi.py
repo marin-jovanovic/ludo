2f11d08571e6334d0db3e411a2520878c763351f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-19 10:32:54 +0200

    backend wsgi.py - wip: ws observer

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,35 +9,49 @@ import os
 import time
 from threading import Thread
 
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.wsgi import get_wsgi_application
 
 # This will set production as default, but we must still set it with an
 # ENV on heroku to ensure that the migrate command runs agains the correct DB
+from django.urls import path
+
+from backend.api.consumer import PracticeConsumer
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
 print("wsgi")
-print(80 * "-")
+# print(80 * "-")
 application = get_wsgi_application()
 
-from backend.api.ws.main import init_server
-
-def task():
-    print("task")
-    init_server()
-    # block for a moment
-    # time.sleep(15)
-    # sleep(1)
-    # display a message
-    print('This is from another thread')
-    # users = get_logged_users()
-    # users = await database_sync_to_async(get_logged_users())()
-
-    # print(f"{users=}")
-
-
-thread = Thread(target=task)
-thread.start()
-
+# application = ProtocolTypeRouter(
+#     {
+#         "http": get_asgi_application(),
+#         "websocket": AllowedHostsOriginValidator(
+#             AuthMiddlewareStack(
+#                 URLRouter([
+#                     path('whole1/', PracticeConsumer().as_asgi())
+#                     # re_path(r"^front(end)/$",
+#                     #         consumers.AsyncChatConsumer.as_asgi()),
+#                 ])
+#             )
+#         ),
+#         # Just HTTP for now. (We can add other protocols later.)
+#     }
+# )
 #
-# init_server()
-# print("after")
+# from backend.api.ws.main import init_server
+#
+# def task():
+#     print("task")
+#     init_server()
+#     print('This is from another thread')
+#
+#
+
+# thread = Thread(target=task)
+# thread.start()
