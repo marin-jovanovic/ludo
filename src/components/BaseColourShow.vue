680f5961f6c561c68b41ed7ccf7644ea4d1c6339
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 19:07:41 +0200

    src components BaseColourShow.vue - wip: master detail

---
+++ src/components/BaseColourShow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col">
+    <input
+      type="color"
+      class="form-control form-control-color"
+      value="this.colour"
+    />
+  </div>
+</template>
+    
+    <script>
+export default {
+  props: {
+    colour: String,
+  },
+  data() {
+    return {
+      //   colour: "#259854",
+    };
+  },
+  mounted() {
+    console.log("mounted color", this.colour);
+  },
+  methods: {},
+};
+</script>
+    
