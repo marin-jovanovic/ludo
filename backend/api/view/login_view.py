b99b4901ff14f00789dba607ad25f8086b5f48c0
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 19:07:41 +0200

    backend api view login_view.py - wip: master detail

--- backend/api/view/login_view.py
+++ backend/api/view/login_view.py
@@ -1,8 +1,5 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
-
-# from backend.api.auth.main import create_user
-from backend.api.cqrs_c.users import auth_user
 from backend.api.view.comm import get_auth_ok_response_template
 
 
@@ -10,12 +7,6 @@ class LoginView(APIView):
 
     def post(self, request, username):
         # return access token for provided username and password
-        print(f"post {username=}")
-
-        password = request.headers["Authorization"].split(" ")[1].split(":")[0]
-        print(f"{password=}")
 
         response = get_auth_ok_response_template(request)
-        response["payload"] = auth_user(username, password)
-
         return JsonResponse(response)
