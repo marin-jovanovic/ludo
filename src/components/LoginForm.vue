fa8e4d1a308fe2a407f97dd1c8aba4ab4cbd62bd
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 19:07:41 +0200

    src components LoginForm.vue - wip: master detail

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,34 +1,43 @@
 <template>
-    <form @submit.prevent="handleSubmit">
-        <div v-if="error" class="alert alert-danger">{{ error }}</div>
-        <div class="form-outline mb-4">
-            <label class="form-label" for="form2Example17">Username</label>
-            <input type="text" id="form2Example17" v-model="username" class="form-control form-control-lg"
-                :class="{ 'is-invalid': submitted && !username }" />
+  <form @submit.prevent="handleSubmit">
+    <div v-if="error" class="alert alert-danger">{{ error }}</div>
+    <div class="form-outline mb-4">
+      <label class="form-label" for="form2Example17">Username</label>
+      <input
+        type="text"
+        id="form2Example17"
+        v-model="username"
+        class="form-control form-control-lg"
+        :class="{ 'is-invalid': submitted && !username }"
+      />
 
-            <div v-show="submitted && !username" class="invalid-feedback">
-                Username is required
-            </div>
-        </div>
+      <div v-show="submitted && !username" class="invalid-feedback">
+        Username is required
+      </div>
+    </div>
 
-        <div class="form-outline mb-4">
-            <label class="form-label" for="form2Example27">Password</label>
+    <div class="form-outline mb-4">
+      <label class="form-label" for="form2Example27">Password</label>
 
-            <input type="password" id="form2Example27" v-model="password"
-                :class="{ 'is-invalid': submitted && !password }" class="form-control form-control-lg" />
+      <input
+        type="password"
+        id="form2Example27"
+        v-model="password"
+        :class="{ 'is-invalid': submitted && !password }"
+        class="form-control form-control-lg"
+      />
 
-            <div v-show="submitted && !password" class="invalid-feedback">
-                Password is required
-            </div>
-        </div>
+      <div v-show="submitted && !password" class="invalid-feedback">
+        Password is required
+      </div>
+    </div>
 
-        <div class="pt-1 mb-4">
-            <button class="btn btn-dark btn-lg btn-block" :disabled="loading">
-                Login
-            </button>
-
-        </div>
-    </form>
+    <div class="pt-1 mb-4">
+      <button class="btn btn-dark btn-lg btn-block" :disabled="loading">
+        Login
+      </button>
+    </div>
+  </form>
 </template>
   
   
@@ -36,41 +45,39 @@
 import { router } from "../router/router";
 import { apiAuth } from "../scripts/api/auth";
 export default {
-    data() {
-        return {
-            username: "a",
-            password: "a",
-            submitted: false,
-            loading: false,
-            returnUrl: "",
-            error: "",
-        };
-    },
-    created() {
-        apiAuth.logout();
-        this.returnUrl = this.$route.query.returnUrl || "/";
-    },
-    methods: {
-        handleSubmit() {
-            this.submitted = true;
-            const { username, password } = this;
-            if (!(username && password)) {
-                return;
-            }
-            this.loading = true;
-            apiAuth.login(username, password).then(
-                () => {
-                    router.push(this.returnUrl);
-                    this.$store.dispatch("setUsername", username);
-                },
-                (error) => {
-                    this.error = "invalid credentials";
-                    this.error = error;
-                    this.loading = false;
-                }
-            );
+  data() {
+    return {
+      username: "d",
+      password: "d",
+      submitted: false,
+      loading: false,
+      returnUrl: "",
+      error: "",
+    };
+  },
+  created() {
+    apiAuth.logout();
+    this.returnUrl = this.$route.query.returnUrl || "/";
+  },
+  methods: {
+    handleSubmit() {
+      this.submitted = true;
+      const { username, password } = this;
+      if (!(username && password)) {
+        return;
+      }
+      this.loading = true;
+      apiAuth.login(username, password).then(
+        () => {
+          router.push(this.returnUrl);
         },
+        (error) => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
     },
+  },
 };
 </script> 
       
