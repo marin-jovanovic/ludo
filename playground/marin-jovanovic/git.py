d9d9c083789bc0af857c09e87c52fad5a434dec1
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 10:28:39 +0200

    playground marin-jovanovic git.py - wip: misc

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -19,7 +19,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: rebuild req"
+    comment = "wip: misc"
 
 
     if same_comment:
