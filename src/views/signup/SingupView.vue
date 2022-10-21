b9eb6df888d49737b9918f8e5ccde091249500ed
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 10:28:39 +0200

    src views signup  - wip: misc

---
+++ src/views/signup/SingupView.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <BasePublicTemplate>
+        <SignUpForm></SignUpForm>
+        <TheLinks></TheLinks>
+    </BasePublicTemplate>
+
+</template>
+
+
+<script>
+import SignUpForm from "@/components/SignUpForm.vue";
+import BasePublicTemplate from "@/components/BasePublicTemplate.vue";
+import TheLinks from "@/components/TheLinks.vue";
+
+export default {
+    data() {
+        return {};
+    },
+    components: { SignUpForm, BasePublicTemplate, TheLinks }
+};
+</script> 
