65e006d77f9ac00df7de4349ca58af6cb1adcdce
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 19:07:41 +0200

    backend api apps.py - wip: master detail

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -5,9 +5,3 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'backend.api'
-
-    def ready(self):
-        print("ready")
-
-
-
