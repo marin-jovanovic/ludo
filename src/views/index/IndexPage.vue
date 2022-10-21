6c46f48c0249f54d8f0b9baf5d109b43eb17f35f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 19:07:41 +0200

    src views index IndexPage.vue - wip: master detail

--- src/views/index/IndexPage.vue
+++ src/views/index/IndexPage.vue
@@ -1,52 +1,85 @@
 <template>
   <BaseUserTemplate>
-    <div>index page</div>
+    <div class="container mt-2">
+      <div class="row">
+        <BasePortfolioList
+          @selectUpdate="updateSelectedList"
+          @increase-by="updateSelectedForChart"
+          :selectedForChart="selected"
+        ></BasePortfolioList>
+      </div>
+    </div>
+
+    <BaseMessage ref="message"></BaseMessage>
   </BaseUserTemplate>
 </template>
 
 <script>
 import { useToast } from "vue-toastification";
 import BaseUserTemplate from "@/components/BaseUserTemplate.vue";
+import BaseMessage from "@/components/BaseMessage.vue";
+import BasePortfolioList from "@/components/BasePortfolioList.vue";
 
 export default {
   setup() {
     const toast = useToast();
-    toast("notif test");
+    // toast("notif test");
     return { toast };
+  },
+
+  mounted() {
+    // this.toast("hello again");
+    // console.log("mounted");
+    // console.log("mounted", this.selected);
+    // this.$refs.message.showMessage(true, "hello", "world");
   },
   data() {
     return {
-      notification: {
-        show: false,
-        position: ["bottom", "right"],
-      },
+      selected: {},
+      selectedList: {},
     };
   },
   methods: {
-    successNotify(content) {
-      this.toast.success(content, {
-        timeout: 2000,
-      });
-    },
-    showMessage(result, successMessage, errorMessage) {
-      if (result) {
-        if (successMessage) {
-          this.toast.success(successMessage, {
-            timeout: 2000,
-          });
-        }
+    updateSelectedList(pl) {
+      console.log("checkobx tick", pl);
+      console.log("base");
+
+      let o = {
+        portfolio: pl.portfolio,
+        section: pl.locationPayload.oldSection,
+        type: pl.locationPayload.oldType,
+      };
+      console.table(o);
+
+      let isSelected = pl.locationPayload.isSelected;
+      console.log(isSelected);
+
+      if (!(o.portfolio in this.selectedList)) {
+        console.log("new portfolio");
+        this.selectedList[o.portfolio] = new Set();
+      }
+
+      if (isSelected) {
+        this.selectedList[o.portfolio].add({
+          section: o.section,
+          type: o.type,
+        });
       } else {
-        if (errorMessage) {
-          this.toast.error(errorMessage, {
-            timeout: 2000,
-          });
-        }
+        this.selectedList[o.portfolio].delete({
+          section: o.section,
+          type: o.type,
+        });
+      }
+
+      for (const [key, value] of Object.entries(this.selectedList)) {
+        console.log(key);
+        console.table(value);
       }
     },
-    notify() {
-      this.showMessage(false, "t", "f");
+    showSelected() {
+      console.log(this.selected);
     },
   },
-  components: { BaseUserTemplate },
+  components: { BaseUserTemplate, BaseMessage, BasePortfolioList },
 };
 </script>
