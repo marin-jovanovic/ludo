966f923989c8d3b949a1af0b94e2d1d710e71fab
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 10:28:40 +0200

    backend api view signup_view.py - wip: misc

--- backend/api/view/signup_view.py
+++ backend/api/view/signup_view.py
@@ -12,9 +12,18 @@ class SignUpView(APIView):
         # input: username, password
         # output: create user in db
 
-        password = request.headers['password']
+        # password = request.headers['password']
+        # password = request.password
 
         response = get_auth_ok_response_template(request)
-        response["payload"]["status"] = create_user(username, password)
+        response["payload"]["status"] = True
+        response["payload"]["payload"] = {
+            'username': request.username,
+            'settings1': 'r 1',
+            'settings2': 'r 2',
+        }
+        # response["payload"]["payload"] = 'if you can pass middleware you are authenticated'
+
+        # response["payload"]["status"] = create_user(username, password)
 
         return JsonResponse(response)
