34b85ba91e9056fe19d2f6367da4d8e44689cbca
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-21 19:07:41 +0200

    src components TopNavigationBarName.vue - wip: master detail

--- src/components/TopNavigationBarName.vue
+++ src/components/TopNavigationBarName.vue
@@ -1,16 +1,18 @@
 <template>
-    <router-link class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold" :to="{ name: 'index' }">Beyond
-    </router-link>
+  <router-link
+    class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold"
+    :to="{ name: 'index' }"
+    >Ludo
+  </router-link>
 </template>
   
 <script>
 export default {
-    data() {
-        return {};
-    },
+  data() {
+    return {};
+  },
 };
 </script>
   
 <style>
-
 </style>
