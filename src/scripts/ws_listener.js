609cd9fd3b60338e80396eb3588b6eabff81da6c
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-22 20:19:07 +0200

    src scripts ws_listener.js - impl: lobby master detail

---
+++ src/scripts/ws_listener.js
@@ -0,0 +1,82 @@
+class WebSocketListener {
+    constructor(url, onMessageAction) {
+
+        self.socket = new WebSocket(url);
+
+        
+        self.socket.onopen = function() {
+            console.log("[ws open] Connection established");   
+        };
+        
+        self.socket.onmessage = function(event) {
+            console.log("[ws message]");
+
+            console.log(event.data, typeof(event.data))
+
+
+            let message = JSON.parse(event.data);
+            
+            console.log(message.message)
+
+            onMessageAction(message);
+
+            // if (message.message === "getUserActive") {
+            //     console.log("ws get user active")
+                
+            //     console.log(message.args);
+            //     // document.querySelector("#au").innerHTML = Object.keys(message.args)
+           
+            // } else {
+            //     console.log("other msg")
+            // }
+        
+        };
+        
+        self.socket.onclose = function(event) {
+            if (event.wasClean) {
+                console.log(`[ws close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            } else {
+                // e.g. server process killed or network down
+                // event.code is usually 1006 in this case
+                console.log('[ws close] Connection died');
+            }
+        };
+        
+        self.socket.onerror = function(error) {
+            console.log("err", error)
+            console.log(`[ws error] ${error.message}`);
+        };
+        
+    }
+
+    start() {
+        console.log("start ws listenrs")
+    }
+
+    sendMessage(payload) {
+        self.socket.send(payload)
+    }
+
+}
+
+// function getUserActive(message) {
+//     console.log("ws get user active")
+//     console.log(message)            
+
+//     // console.log(message.args);
+
+// }
+
+// let url = "ws://127.0.0.1:8000/whole1/";
+
+// let wsGetUserActive = new WebSocketListener(url, getUserActive);
+
+export const wsListeners = {
+    // createGame,
+    // getGames,
+    // leaveGame,
+    // joinGame
+    // wsGetUserActive,
+    WebSocketListener
+
+}
