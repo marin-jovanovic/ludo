491138de2155b07a7e5d49c6230d7c4eaa23cdb8
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-22 20:19:07 +0200

    backend asgi.py - impl: lobby master detail

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,19 +1,14 @@
-"""
-WSGI config for project project.
-It exposes the WSGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path
-from backend.api.consumer import PracticeConsumer
+from backend.api.consumer import  Consumer, GameConsumer
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from backend.api.model.game import games_notifier
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
+
 
 application = ProtocolTypeRouter(
     {
@@ -21,7 +16,8 @@ application = ProtocolTypeRouter(
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
                 URLRouter([
-                    path('whole1/', PracticeConsumer().as_asgi())
+                    path('whole1/', GameConsumer().as_asgi())
+                    # path('whole1/', PracticeConsumer().as_asgi())
                 ])
             )
         ),
