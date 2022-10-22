5215658accd23572a22ad147fa0d582ed9d70535
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-23 01:35:17 +0200

    playground marin-jovanovic sql drop.sh - impl: messaging; ws updating

--- playground/marin-jovanovic/sql/drop.sh
+++ playground/marin-jovanovic/sql/drop.sh
@@ -1,10 +1,10 @@
 sudo su postgres
 psql
-\c beyond
+\c ludo
 SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'ludo';
 \q
 psql
 // now connected to postgres
-drop database if exists beyond;
+drop database if exists ludo;
 \q
 exit
