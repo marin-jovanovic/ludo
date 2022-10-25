373b0c3b04b6212162513f8ce23ff28ef684a427
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 00:29:08 +0200

    backend api view game_view.py - wip: game view

--- backend/api/view/game_view.py
+++ backend/api/view/game_view.py
@@ -8,8 +8,10 @@ from rest_framework.views import APIView
 from backend.api.cqrs_c.users import auth_user
 from backend.api.cqrs_c.game import create_game, leave_game, join_game, \
     get_games, get_specific_game
+from backend.api.model.player_order import get_player_order
 from backend.api.view.comm import get_auth_ok_response_template
-from backend.api.model.game_log import add_entry
+from backend.api.cqrs_c.game_log import add_entry
+from backend.api.game.game import determine_order, get_config
 
 class GameView(APIView):
 
@@ -19,34 +21,18 @@ class GameView(APIView):
         response = get_auth_ok_response_template(request)
         response['payload'] = get_specific_game(name)
 
-        # response['payload'] = {
-        #     "status": True,
-        #     "payload": {
-        #         # "turn":
-        #         # "init order": [0, 2, 3, 5],
-        #
-        #
-        #     }
-        # }
-        #     # get_games()
-
-
-
         return JsonResponse(response)
 
-    # GET 	Retrieve information about the REST API resource
-    # POST 	Create a REST API resource
-    # PUT 	Update a REST API resource
-    # DELETE 	Delete a REST API resource or related component
-
-    # todo observers
-
     def post(self, request, name):
+        response = get_auth_ok_response_template(request)
 
         creator_username = request.username
 
         unquoted_body = urllib.parse.unquote(request.body)
         body = urllib.parse.parse_qs(unquoted_body)
+
+        print(f"{request.body=}")
+        print(f"{request.data=}")
 
         try:
             token = body["token"][0]
@@ -55,9 +41,60 @@ class GameView(APIView):
             token = request.data["token"]
             action = request.data["action"]
 
+        if action == "start":
+            def driver():
+
+                # todo
+                print("determine users")
+
+                # todo this is joining order, not playing order, change this
+                order = get_player_order(name)
+                print(f"{order=}")
+                if not order["status"]:
+                    print("get game err")
+                    return order
+                else:
+                    o_o = order["payload"]
+
+                print(80 * "-")
+                print(o_o)
+
+                from rest_framework.renderers import JSONRenderer
+
+                json = JSONRenderer().render(o_o)
+                print(f"{json=}")
+                for i in o_o:
+                    print(i, i.index)
+
+                m_join_order_to_username = {i.index: i.player_id.username for i in o_o}
+                print(f"{m_join_order_to_username=}")
+
+                game_conf = get_config()
+
+                order = determine_order(
+                    game_conf['number of players'],
+                    game_conf['choice: highest; order'],
+                    game_conf['choice: clockwise; anticlockwise'],
+                    game_conf['flag: tie in order'],
+                )
+                for i in order:
+                    print(f"befor {i=}")
+                    i["game"] = name
+                    i["player"] = m_join_order_to_username[i["player"]]
+                    print(f"after {i=}")
+
+                    r = add_entry(**i)
+                    if not r["status"]:
+                        return r
+
+            r = driver()
+            response["payload"] = r
+            return JsonResponse(response)
+
+
         dice_result = None
 
-        response = get_auth_ok_response_template(request)
         response["payload"] = add_entry(name, creator_username, token, dice_result, action)
 
         return JsonResponse(response)
+
