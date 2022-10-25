8096c362f388f97687594108ba891ab9f9645b0e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-25 18:50:41 +0200

    backend api view game_view.py - impl: user game action log

--- backend/api/view/game_view.py
+++ backend/api/view/game_view.py
@@ -6,17 +6,29 @@ from rest_framework.views import APIView
 
 # from backend.api.auth.main import create_user
 from backend.api.cqrs_c.users import auth_user
-from backend.api.model.game import create_game, leave_game, join_game, get_games
+from backend.api.cqrs_c.game import create_game, leave_game, join_game, \
+    get_games, get_specific_game
 from backend.api.view.comm import get_auth_ok_response_template
-
+from backend.api.model.game_log import add_entry
 
 class GameView(APIView):
 
-    def get(self, request):
+    def get(self, request, name):
 
         print("get games")
         response = get_auth_ok_response_template(request)
-        response['payload'] = get_games()
+        response['payload'] = get_specific_game(name)
+
+        # response['payload'] = {
+        #     "status": True,
+        #     "payload": {
+        #         # "turn":
+        #         # "init order": [0, 2, 3, 5],
+        #
+        #
+        #     }
+        # }
+        #     # get_games()
 
 
 
@@ -36,48 +48,16 @@ class GameView(APIView):
         unquoted_body = urllib.parse.unquote(request.body)
         body = urllib.parse.parse_qs(unquoted_body)
 
-        # print(f"{request.data=}")
-        # print(f"{body=}")
-
         try:
-            capacity = body["capacity"][0]
+            token = body["token"][0]
+            action = body["action"][0]
         except KeyError:
-            capacity = request.data["capacity"]
+            token = request.data["token"]
+            action = request.data["action"]
 
-        print(f"{creator_username=}")
-        print(f"{capacity=}")
-
-        response = get_auth_ok_response_template(request)
-        response["payload"] = create_game(creator_username, name, capacity)
-
-        return JsonResponse(response)
-
-    def put(self, request, name):
-        print("put lobby", name)
-        username = request.username
-
-        unquoted_body = urllib.parse.unquote(request.body)
-        body = urllib.parse.parse_qs(unquoted_body)
+        dice_result = None
 
         response = get_auth_ok_response_template(request)
-
-        print(f"{body=}")
-        print(f"{request.data=}")
-
-        if "leave" in request.data:
-            # leave = body["leave"][0]
-            #
-            # if leave:
-            print("leave")
-            response["payload"] = leave_game(name, username)
-            print("res payload", response)
-
-        if "join" in request.data:
-            # join = body["join"][0]
-            #
-            # if join:
-            print("join")
-            response["payload"] = join_game(name, username)
+        response["payload"] = add_entry(name, creator_username, token, dice_result, action)
 
         return JsonResponse(response)
-
