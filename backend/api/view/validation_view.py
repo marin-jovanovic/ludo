47c905ff1808b6880bc5aca8c8998fa1f2f9b7a3
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 00:29:08 +0200

    backend api view validation_view.py - wip: game view

--- backend/api/view/validation_view.py
+++ backend/api/view/validation_view.py
@@ -1,8 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-# from backend.api.auth.main import create_user
-# from backend.api.cqrs_c.users import is_access_token_correct
 from backend.api.cqrs_q.users import is_access_token_correct
 from backend.api.view.comm import get_auth_ok_response_template
 
@@ -10,12 +8,11 @@ from backend.api.view.comm import get_auth_ok_response_template
 class ValidationView(APIView):
 
     def post(self, request, username):
-        # check if access token is correct
 
         print(request.headers)
-        access_token = request.headers['accessToken']
+        # access_token = request.headers['accessToken']
 
         response = get_auth_ok_response_template(request)
-        response["payload"] = is_access_token_correct(username, access_token)
+        # response["payload"] = is_access_token_correct(username, access_token)
 
         return JsonResponse(response)
