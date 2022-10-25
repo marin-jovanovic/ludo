7ca64adb9ff9e4ead4f9878313279afc19e4b54e
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 00:29:08 +0200

    backend api model game_log.py - wip: game view

--- backend/api/model/game_log.py
+++ backend/api/model/game_log.py
@@ -1,4 +1,3 @@
-from django.apps import apps
 from django.db import models
 from backend.api.model.users import get_user_model
 from backend.api.model.game import _get_game_model
@@ -32,36 +31,3 @@ class GameLog(models.Model):
     # todo extract in separate table
     action = models.TextField()
 
-from backend.api.cqrs_q.game import __get_game
-from backend.api.cqrs_c.users import get_user
-from django.db.models import Max
-
-def add_entry(game, player, token, dice_result, action):
-    r = __get_game(game)
-    if r["status"]:
-        game_o = r["payload"]
-    else:
-        return r
-
-    r = get_user(player)
-    if r["status"]:
-        user_o = r["payload"]
-    else:
-        return r
-
-    instr_id = GameLog.objects.filter(game=game_o).aggregate(Max("instruction_id"))["instruction_id__max"]
-
-    if not isinstance(instr_id, int):
-        instr_id = -1
-
-    e = GameLog(
-        game=game_o,
-        instruction_id=instr_id+1,
-        player=user_o,
-        token=token,
-        dice_result=dice_result,
-        action=action,
-    )
-    e.save()
-
-    return {"status": True}
