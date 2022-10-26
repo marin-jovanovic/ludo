9578c45e367e938f4657618853e2fe732eb82a49
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 15:02:33 +0200

    src scripts api board.js - wip: ref game driver

---
+++ src/scripts/api/board.js
@@ -0,0 +1,21 @@
+import { apiCalls } from './comm';
+
+
+async function getBoard(type, resource) {
+    return await apiCalls.handleNewResponse(
+        await apiCalls.api.get(
+            `board/${type}/${resource}`,
+            apiCalls.getAuthenticationHeader()
+        )
+    );
+}
+
+
+
+
+
+export const apiBoard = {
+getBoard,
+
+
+}
