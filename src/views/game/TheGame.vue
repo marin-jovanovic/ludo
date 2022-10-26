02fd34b5dfa2da877261b24232cd315c1a269aff
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 13:46:32 +0200

    src views game TheGame.vue - impl: integrate board

---
+++ src/views/game/TheGame.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button id="ulul">ul ul</button>
+    <button id="ulur">ul ur</button>
+    <button id="uldl">ul dl</button>
+    <button id="uldr">ul dr</button>
+    <br />
+    <button id="rulul">ret ul ul</button>
+    <button id="rulur">ret ul ur</button>
+    <button id="ruldl">ret ul dl</button>
+    <button id="ruldr">ret ul dr</button>
+
+    <hr />
+
+    <canvas> </canvas>
+  </div>
+</template>
+    
+    
+  <script>
+import { ludo } from "@/views/game/ludo/scripts/index.js";
+
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    ludo.startup();
+  },
+  methods: {},
+};
+</script> 
+    
+    <style>
+@import "ludo/index.css";
+</style>
+
+  
+  
