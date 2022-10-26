c5e90f404b82a07e8911e1c19b887a39586b8986
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 02:04:57 +0200

    backend api view lobby_view.py - impl: write to db playing order

--- backend/api/view/lobby_view.py
+++ backend/api/view/lobby_view.py
@@ -4,13 +4,14 @@ import urllib
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from backend.api.cqrs_c.game import create_game, leave_game, join_game, \
-    get_specific_game, get_games, in_which_game_is_user
+     get_games, in_which_game_is_user
 from backend.api.view.comm import get_auth_ok_response_template
 
 
 class LobbyView(APIView):
 
     def get(self, request, name=None):
+        # get basic game info
 
         print("get games")
 
@@ -19,8 +20,9 @@ class LobbyView(APIView):
             response['payload'] = get_games()
             response["payload"]["payload"]["inGame"] = in_which_game_is_user(request.username)["payload"]
         else:
+            print("not implemented, no use ? ")
             response = get_auth_ok_response_template(request)
-            response['payload'] = get_specific_game(name)
+            # response['payload'] = get_specific_game(name)
 
         return JsonResponse(response)
 
@@ -28,6 +30,7 @@ class LobbyView(APIView):
     # todo observers
 
     def post(self, request, name):
+        # create game
 
         creator_username = request.username
 
@@ -48,29 +51,20 @@ class LobbyView(APIView):
         return JsonResponse(response)
 
     def put(self, request, name):
-        # print("put lobby", name)
+        # join / leave game
         username = request.username
 
-        unquoted_body = urllib.parse.unquote(request.body)
-        body = urllib.parse.parse_qs(unquoted_body)
+        # unquoted_body = urllib.parse.unquote(request.body)
+        # body = urllib.parse.parse_qs(unquoted_body)
 
         response = get_auth_ok_response_template(request)
 
-        # print(f"{body=}")
-        # print(f"{request.data=}")
-
         if "leave" in request.data:
-            # leave = body["leave"][0]
-            #
-            # if leave:
             print("leave")
             response["payload"] = leave_game(name, username)
             print("res payload", response)
 
         if "join" in request.data:
-            # join = body["join"][0]
-            #
-            # if join:
             print("join")
             response["payload"] = join_game(name, username)
 
