fa83cb9bc092aa9fd202f433b2131c629f6a28a5
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 13:46:33 +0200

    playground marin-jovanovic git.py - impl: integrate board

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -19,7 +19,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: roll"
+    comment = "impl: integrate board"
 
 
     if same_comment:
