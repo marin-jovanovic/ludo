d1e18ec22e911a78730a447c110e662ac77439f2
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 02:04:57 +0200

    playground marin-jovanovic git.py - impl: write to db playing order

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -19,7 +19,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: game view"
+    comment = "impl: write to db playing order"
 
 
     if same_comment:
