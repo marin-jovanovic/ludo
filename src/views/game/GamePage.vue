021b66fca765cca8920a8f51f8c5afef74e4d1c8
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 13:46:33 +0200

    src views game GamePage.vue - impl: integrate board

--- src/views/game/GamePage.vue
+++ src/views/game/GamePage.vue
@@ -1,85 +1,79 @@
 <template>
   <BaseUserTemplate>
     <!-- <hr /> -->
+
     <h1>{{ this.header }}</h1>
     <br />
-
+    <h2>order: {{ this.playingOrder }}</h2>
+    <br />
     <button @click="leaveGame">leave game</button>
-
     <div>
       <div>players:</div>
       <div v-for="i in this.players" :key="i">p: {{ i }}</div>
     </div>
-
     <h1>now playing:</h1>
 
+    <hr />
+
     <div class="row">
-      <div class="col"></div>
       <div class="col">
-        <TheDice ref="dice"></TheDice>
+        <div class="row">
+          <div class="col"></div>
+          <div class="col">
+            <TheDice ref="dice"></TheDice>
+          </div>
+          <div class="col">
+            <button @click="rollDice">roll dice</button>
+          </div>
+        </div>
       </div>
-      <div class="col">
-        <button @click="rollDice">roll dice</button>
-      </div>
+      <div class="col"><TheMessages></TheMessages></div>
     </div>
 
-    <div>
-      <button>move 1</button>
-      <button>move 2</button>
-      <button>move 3</button>
-      <button>move 4</button>
-    </div>
+    <TheGame></TheGame>
 
     <hr />
-
-    <div>
-      token
-
-      <input type="text" v-model="cToken" />
-      <br />
-
-      action
-      <input type="text" v-model="cAction" />
-      <br />
-
-      <button @click="updateGame">submit</button>
-    </div>
-    <hr />
-    <TheMessages></TheMessages>
-
-    <div class="row"></div>
   </BaseUserTemplate>
 </template>
 
 
 <script>
+import TheGame from "./TheGame.vue";
 import TheDice from "./TheDice.vue";
 import BaseUserTemplate from "@/components/BaseUserTemplate.vue";
 import { apiLobby } from "@/scripts/api/lobby";
 import { router } from "@/router/router";
 import TheMessages from "./TheMessages.vue";
 import { apiGame } from "@/scripts/api/game";
+// import { wsListeners } from "@/scripts/ws_listener";
 
 export default {
   data() {
     return {
-      canRoll: true,
+      canRoll: false,
+      rollResult: -1,
+
       username: "",
       header: "",
 
-      // todo rewrite url with this
       gameId: "",
 
       players: {},
       capacity: -1,
 
-      cToken: "",
-      cAction: "",
+      instructionCurrentlyPerforming: -1,
+
+      lastInstructionPerformed: -1,
+
+      playingOrder: [],
     };
   },
   async mounted() {
+    // let url = "ws://127.0.0.1:8000/game/";
+    // new wsListeners.WebSocketListener(url, this.getUserActive);
+    // console.log("ws init");
+
     this.username = sessionStorage.getItem("username");
-    // this.gameId = sessionStorage.getItem("gameId");
 
     this.gameId = this.$route.params.id;
 
@@ -87,73 +81,147 @@ export default {
     if (res["auth"]["status"] && res["payload"]["status"]) {
       let p = res["payload"]["payload"];
 
-      console.table(p);
-
       console.table(p["log"]);
 
-      this.header = p.header;
+      for (const [key, value] of Object.entries(p["log"])) {
+        console.log("instruction", key);
 
-      if (p.turn === this.username) {
-        this.canRoll = true;
-      } else {
-        this.canRoll = false;
+        switch (value.action) {
+          case "goes":
+            this.playingOrder.push(value.username);
+            this.instructionCurrentlyPerforming = key;
+            this.lastInstructionPerformed = key;
+
+            await this.actionPerformed();
+
+            break;
+
+          default:
+            break;
+        }
+
+        if (!value.performed) {
+          if (value.username == this.username) {
+            console.log("your turn");
+
+            switch (value.action) {
+              case "roll":
+                await this.rollDriver(key, value);
+                break;
+
+              default:
+                console.log("not handled action");
+                break;
+            }
+          } else {
+            console.log("waiting for player", value.username);
+
+            switch (value.action) {
+              case "roll":
+                await this.enemyRollDriver();
+                break;
+
+              default:
+                console.log("not handled action");
+                break;
+            }
+
+            console.log("breaking");
+            break;
+          }
+        } else {
+          console.log("this action is performed");
+
+          if (value.username === this.username) {
+            switch (value.action) {
+              case "roll":
+                this.$refs.dice.rollDice(value.diceResult);
+                break;
+              default:
+                console.log("not handled yet");
+            }
+          } else {
+            console.log("someone else performed something");
+          }
+        }
+
+        this.lastInstructionPerformed = key;
+        console.log("last perfromed", this.lastInstructionPerformed);
       }
+
+      this.header = p.header;
 
       this.players = p.players;
       this.capacity = p.capacity;
     } else {
       console.log("err fetching data");
     }
-
-    // for (const [key, value] of Object.entries(this.players)) {
-    //   console.log(key, value);
-    //   console.log("roll player", value);
-    // }
   },
   methods: {
-    async updateGame() {
+    getUserActive(message) {
+      console.log("ws msg received, todo");
+      console.log(message);
+    },
+
+    async enemyRollDriver() {
+      console.log("enemy roll driver");
+    },
+
+    async rollDriver(key, value) {
+      console.log("roll", value.diceResult);
+      this.canRoll = true;
+      this.rollResult = value.diceResult;
+      this.instructionCurrentlyPerforming = key;
+    },
+
+    async updateGame(token, action) {
       let res = await apiGame.updateGame(
         this.gameId,
         this.username,
-        this.cToken,
-        this.cAction
+        token,
+        action
       );
 
-      if (res["auth"]["status"]) {
-        if (!res["payload"]["status"]) {
-          console.log("game leave err");
-        }
-      } else {
-        console.log("err update");
+      if (!(res["auth"]["status"] && res["payload"]["status"])) {
+        console.log("game leave err");
       }
     },
 
     async leaveGame() {
       let res = await apiLobby.leaveGame(this.gameId);
-      if (res["auth"]["status"]) {
-        if (res["payload"]["status"]) {
-          router.push("/");
-        } else {
-          console.log("game leave err");
-        }
+
+      if (res["auth"]["status"] && res["payload"]["status"]) {
+        router.push("/");
+      } else {
+        console.log("[err] leave game");
       }
     },
 
-    rollDice() {
+    async rollDice() {
       if (!this.canRoll) {
         console.log("can not roll");
         return;
       }
 
-      this.$refs.dice.rollDice(5);
+      this.canRoll = false;
+
+      this.$refs.dice.rollDice(this.rollResult);
+
+      await this.actionPerformed();
+    },
+
+    async actionPerformed() {
+      let res = await apiGame.actionPerformed(
+        this.gameId,
+        this.username,
+        this.instructionCurrentlyPerforming
+      );
+
+      if (!(res["auth"]["status"] && res["payload"]["status"])) {
+        console.log("game leave err");
+      }
     },
   },
-  components: { TheDice, BaseUserTemplate, TheMessages },
+  components: { TheDice, BaseUserTemplate, TheMessages, TheGame },
 };
 </script> 
-
-<style>
-.col {
-  border: 1mm solid black;
-}
-</style>
