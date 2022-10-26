f7c626d93ebf1394bb71886af685a4064c673ea5
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-26 15:02:33 +0200

    backend api model game.py - wip: ref game driver

--- backend/api/model/game.py
+++ backend/api/model/game.py
@@ -2,6 +2,7 @@ from django.apps import apps
 from django.db import models
 
 from backend.api.comm.comm import Notifier
+from backend.api.model.users import get_user_model
 
 game_created_notifier = Notifier()
 game_left_notifier = Notifier()
@@ -21,3 +22,34 @@ class Game(models.Model):
 
 def _get_game_model():
     return apps.get_model("api.game")
+
+class Tile(models.Model):
+
+    x = models.IntegerField()
+    y = models.IntegerField()
+
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+class Token(models.Model):
+
+    where = models.ForeignKey(
+        Tile,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    name = models.IntegerField()
+
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+
+def move_token(owner, token_name, step):
+    print(f"move token {owner=} {token_name=} {step=}")
