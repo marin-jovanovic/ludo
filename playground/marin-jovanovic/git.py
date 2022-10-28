1908e42dafeba37f63d2e18f722abbefbdf6e1b8
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-10-28 11:31:41 +0200

    playground marin-jovanovic git.py - ref:

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -19,7 +19,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: ref game driver"
+    comment = "ref:"
 
 
     if same_comment:
