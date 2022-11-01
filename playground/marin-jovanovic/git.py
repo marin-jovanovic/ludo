82bf8780c3db8493e48101cdba0efe1c60fc24e8
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-01 11:56:23 +0100

    playground marin-jovanovic git.py - impl: game replay

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -19,7 +19,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: game business logic"
+    comment = "impl: game replay"
 
 
     if same_comment:
