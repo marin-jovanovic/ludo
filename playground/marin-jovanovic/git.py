2b05df91c1007af66f386c88039efd75a43caca9
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-01 16:10:09 +0100

    playground marin-jovanovic git.py - wip: business logic

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -19,8 +19,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "impl: game replay"
-
+    comment = "wip: business logic"
 
     if same_comment:
         if not comment:
