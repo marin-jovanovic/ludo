852a9c0bb0f592f5e80e4cdc67bf700126f480d7
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-08 23:39:25 +0100

    src views gameReplay TheActions.vue - wip: logging actions

---
+++ src/views/gameReplay/TheActions.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <table>
+            <tr v-for="(row, rowKey) in grid" :key="rowKey">
+                <th class="row-col-label">{{ rowKey + 1 }}</th>
+                <td v-for="(col, colKey) in row" :key="colKey" @click="selectCell(rowKey, colKey)"
+                    :class="{ 'selected': cellSelected(rowKey, colKey) }">
+                    {{ col }}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+    
+    
+<script>
+
+export default {
+    created() {
+        this.initColHead()
+        this.createSpreadSheet()
+    },
+    data() {
+        return {
+            selected: '',
+            grid: [],
+            colHead: [' '],
+            isSelected: false
+        }
+    },
+    methods: {
+        initColHead() {
+            this.colHead.push(...'ABC'.split(''))
+        },
+        createSpreadSheet() {
+            for (let i = 0; i <= 2; i++) {
+                this.grid[i] = []
+                for (let j = 0; j <= 2; j++) {
+                    this.grid[i][j] = false
+                }
+            }
+        },
+        selectCell(row, col) {
+            const newRow = this.grid[row].slice(0)
+            newRow[col] = true
+            console.log('selected', row, col)
+        },
+        cellSelected(row, col) {
+            return (this.grid[row][col] === true)
+        }
+    }
+}
+
+// export default {
+//     data() {
+//         return {};
+//     },
+//     mounted() {
+//     },
+//     methods: {
+
+//     },
+// };
+</script> 
