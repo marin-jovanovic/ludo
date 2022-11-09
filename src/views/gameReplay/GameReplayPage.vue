039fc0cbe864dd3c7facd3c374fc4ef0737b4eb9
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-09 14:44:00 +0100

    src views gameReplay GameReplayPage.vue - wip: ref game ui separation from logic

--- src/views/gameReplay/GameReplayPage.vue
+++ src/views/gameReplay/GameReplayPage.vue
@@ -1,15 +1,14 @@
 <template>
   <BaseUserTemplate>
 
-    <TheActions></TheActions>
-
-    <button @click="test">test: generate random game</button>
+    <TheTest></TheTest>
 
     <input @change="sliderUpdate" type="range" min="0" max="5000" v-model="this.slider" />
 
     <hr />
 
     <div class="row">
+
       <div class="col" style="border: 2px solid black">
         <TheGame ref="game" style="border: 2px solid black"></TheGame>
       </div>
@@ -18,9 +17,11 @@
         <div>turn:</div>
         <TheDice ref="dice"></TheDice>
       </div>
+
     </div>
 
     <hr />
+
   </BaseUserTemplate>
 </template>
   
@@ -28,12 +29,11 @@
 <script>
 import TheGame from "./../game/TheGame.vue";
 import TheDice from "./../game/TheDice.vue";
+import TheTest from "./TheTest.vue";
 
 import BaseUserTemplate from "@/components/BaseUserTemplate.vue";
 
 import { apiGame } from "@/scripts/api/game";
-import { apiBoard } from "@/scripts/api/board";
-import TheActions from "./TheActions.vue";
 
 export default {
   data() {
@@ -42,12 +42,6 @@ export default {
 
       username: "",
       gameId: "",
-
-      canRoll: false,
-      rollResult: -1,
-
-      instructionCurrentlyPerforming: -1,
-      lastInstructionPerformed: -1,
     };
   },
 
@@ -67,51 +61,63 @@ export default {
 
       pp = Object.assign({}, ...swapped);
 
-      for (const value of Object.values(p["log"])) {
-        // console.log("instruction", key);
+      window.addEventListener('load', () => {
+        console.log('load');
+        (async () => {
+          for (const value of Object.values(p["log"])) {
 
-        switch (value.action) {
-          case "roll":
-            this.$refs.dice.rollDice(value.diceResult);
-            break;
+            switch (value.action) {
+              case "roll":
+                this.$refs.dice.rollDice(value.diceResult);
+                break;
 
-          case "goes":
-            console.log("order ", value.username);
-            break;
+              case "goes":
+                console.log("order ", value.username);
+                break;
 
-          case "move":
-            this.$refs.game.movePosition({
-              player: pp[value.username],
-              token: value.token,
-              jumpCount: value.diceResult,
-            });
+              case "move":
+                this.$refs.game.movePosition({
+                  player: pp[value.username],
+                  token: value.token,
+                  jumpCount: value.diceResult,
+                });
 
-            await this.sleep();
+                // await this.sleep();
+                await new Promise((r) => setTimeout(r, this.slider));
 
-            break;
+                break;
 
-          case "won":
-            console.log("won", value.username);
-            break;
+              case "won":
+                console.log("won", value.username);
+                break;
 
-          case "eaten":
-            this.$refs.game.restartToken({
-              player: pp[value.username],
-              token: value.token,
-            });
+              case "eaten":
+                this.$refs.game.restartToken({
+                  player: pp[value.username],
+                  token: value.token,
+                });
 
-            await this.sleep();
-            break;
+                // await this.sleep();
+                await new Promise((r) => setTimeout(r, this.slider));
 
-          default:
-            break;
-        }
-      }
+                break;
+
+              default:
+                break;
+            }
+          }
+
+        })();
+
+      })
+
+
     } else {
       console.log("err fetching data");
     }
   },
   methods: {
+
     async sleep() {
       await new Promise((r) => setTimeout(r, this.slider));
     },
@@ -120,134 +126,8 @@ export default {
       console.log("slider", this.slider);
     },
 
-    async test() {
-      // actionPerformed(game, player, instruction_id)
-
-      let res = await apiGame.actionPerformed(
-        this.gameId,
-        this.username,
-        "test"
-        // this.instructionCurrentlyPerforming
-      );
-
-      if (!(res["auth"]["status"] && res["payload"]["status"])) {
-        console.log("game leave err");
-      }
-
-      // res = await apiGame.getGame(this.gameId);
-
-      // if (res["auth"]["status"] && res["payload"]["status"]) {
-      //   let p = res["payload"]["payload"];
-
-      //   console.table(p["log"]);
-
-      //   for (const [key, value] of Object.entries(p["log"])) {
-      //     console.log("instruction", key);
-
-      //     if (value.action === "goes") {
-      //       this.$refs.info.addToPlayingOrder(value.username);
-      //     }
-
-      //     if (value.performed) {
-      //       await this.performed(value);
-      //     } else {
-      //       await this.notPefrormed(value, key);
-      //       break;
-      //     }
-      //   }
-      // } else {
-      //   console.log("err fetching data");
-      // }
-    },
-
-    async notPefrormed(value, key) {
-      // for not performed instructions driver
-
-      if (value.username == this.username) {
-        console.log("your turn");
-
-        switch (value.action) {
-          case "roll":
-            await this.notPerfrormedMyRoll(key, value);
-            break;
-
-          default:
-            console.log("not handled action");
-            break;
-        }
-      } else {
-        console.log("waiting for player", value.username);
-
-        switch (value.action) {
-          case "roll":
-            await this.notPerformedEnemyRoll();
-            break;
-
-          default:
-            console.log("not handled action");
-            break;
-        }
-
-        console.log("breaking");
-        // break;
-      }
-    },
-    async performed(value) {
-      // for perfromred instructions driver
-
-      console.log("this action is performed");
-
-      if (value.username === this.username) {
-        switch (value.action) {
-          case "roll":
-            break;
-          default:
-            console.log("not handled yet");
-        }
-      } else {
-        console.log("someone else performed something");
-      }
-    },
-
-    async board() {
-      // for test
-      let res = await apiBoard.getBoard("1", "startPool");
-
-      console.log(res["payload"]["payload"]);
-
-      if (!(res["auth"]["status"] && res["payload"]["status"])) {
-        console.log("game leave err");
-      }
-    },
-
-    async notPerformedEnemyRoll() {
-      console.log("enemy roll driver");
-    },
-
-    async notPerfrormedMyRoll(key, value) {
-      this.canRoll = true;
-      this.rollResult = value.diceResult;
-      this.instructionCurrentlyPerforming = key;
-    },
-
-    // async
-
-    async rollDice() {
-      // runner when user clicks to roll
-
-      if (!this.canRoll) {
-        console.log("can not roll");
-        return;
-      }
-
-      this.canRoll = false;
-
-      this.$refs.dice.rollDice(this.rollResult);
-
-      await this.actionPerformed();
-    },
   },
-  components: { TheDice, BaseUserTemplate, TheGame, TheActions },
+  components: { TheDice, BaseUserTemplate, TheGame, TheTest },
 };
 </script> 
   
