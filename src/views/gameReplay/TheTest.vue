549929dd5bb0a69cd5505d0ee43d480c260bc755
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-09 14:43:59 +0100

    src views gameReplay TheTest.vue - wip: ref game ui separation from logic

---
+++ src/views/gameReplay/TheTest.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="test">test: generate random game</button>
+
+        <button @click="gostpnti">generate only start till player needs to interact</button>
+    </div>
+</template>
+    
+    
+<script>
+import { apiGame } from "@/scripts/api/game";
+
+export default {
+    data() {
+        return {};
+    },
+    mounted() {
+    },
+    methods: {
+        async performAction(action) {
+            let res = await apiGame.actionPerformed(
+                this.gameId,
+                this.username,
+                action
+            );
+
+            if (!(res["auth"]["status"] && res["payload"]["status"])) {
+                console.log("game leave err");
+            }
+
+        },
+
+        async gostpnti() {
+            this.performAction('generatestart');
+        },
+
+        async test() {
+            this.performAction('test');
+        },
+    },
+};
+</script> 
