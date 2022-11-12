a43fbbc87d52ec09ddda1ca33c018ee8c9acbbfd
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-12 23:02:13 +0100

    src views game ludo scripts layers.js - wip: separate ui & bl

--- src/views/game/ludo/scripts/layers.js
+++ src/views/game/ludo/scripts/layers.js
@@ -1,6 +1,3 @@
-// import { Pellet } from "./pellet.js"
-// import { PowerUp } from "./powerup.js"
-// import { createImage } from "./image.js"
 import { Token } from "./token.js"
 const mapping = {
     "-": "pipeHorizontal.png",
@@ -26,19 +23,9 @@ function importAll(r) {
     return r.keys().map(r);
 }
 
-// const pathPrefix = "/public/img/"
-
-var loc = window.location.pathname;
-console.log("loc", loc)
-var dir = loc.substring(0, loc.lastIndexOf('/'));
-console.log("ddir", dir)
-
 function getBoundaries({ map, Boundary }) {
 
-    // console.log("-------")
     let images = importAll(require.context('/public/img/', false, /\.(png|jpe?g|svg)$/));
-
-    // console.log("img", images)
 
     let cloudImages = {};
 
@@ -47,14 +34,10 @@ function getBoundaries({ map, Boundary }) {
         cloudImage.src = images[i];
 
         let name = cloudImage.src.split("/img/")[1].split(".")[0]
-        // console.log(name)
-        // console.log(cloudImage.src)
         cloudImages[name] = cloudImage
-        // cloudImages.push(cloudImage);
     }
 
-    // console.table(cloudImages);
-    // return cloudImages;
+
 
     let boundaries = [];
 
@@ -63,7 +46,6 @@ function getBoundaries({ map, Boundary }) {
 
             if (symbol in mapping) {
 
-                // console.log(mapping[symbol])
 
                 boundaries.push(
                     new Boundary({
@@ -72,11 +54,10 @@ function getBoundaries({ map, Boundary }) {
                             y: Boundary.height * i
                         },
                         image: cloudImages[mapping[symbol].split(".")[0]]
-                        // image: createImage(pathPrefix + mapping[symbol])
+                    
                     })
                 )
 
-                // console.log(createImage(pathPrefix + mapping[symbol]))
 
             }
 
@@ -94,7 +75,17 @@ let mappings = {
     r: 'yellow',
 }
 
-function mapTokens({ map, Boundary, colour }) {
+function remapPosition(i, j, Boundary) {
+    return  {
+        x: j * Boundary.width + Boundary.width / 2,
+        y: i * Boundary.height + Boundary.height / 2
+    }
+
+}
+
+
+
+function mapTokens({ map, Boundary, colour , subscriber}) {
 
 
     let tokens = {};
@@ -106,14 +97,14 @@ function mapTokens({ map, Boundary, colour }) {
             if (symbol in mappings && mappings[symbol] === colour) {
 
                 let newToken = new Token({
-                  
-                    position: {
-                        x: j * Boundary.width + Boundary.width / 2,
-                        y: i * Boundary.height + Boundary.height / 2
-                    },
+
+                    position: remapPosition(i,j, Boundary),
                     colour: mappings[symbol],
-                    state: -1
+                    state: -1,
+                    // notifier: notifier
                 });
+
+                newToken.subscribe(subscriber);
 
                 tokens[c] = newToken;
                 c++;
@@ -127,43 +118,11 @@ function mapTokens({ map, Boundary, colour }) {
 
 }
 
-function getTokens({ map, Boundary }) {
 
-    let tokens = {};
-
-    map.forEach((row, i) => {
-        row.forEach((symbol, j) => {
-
-            if (symbol in mappings) {
-
-                let newToken = new Token({
-                    position: {
-                        x: j * Boundary.width + Boundary.width / 2,
-                        y: i * Boundary.height + Boundary.height / 2
-                    },
-                    colour: mappings[symbol],
-                    state: 'start'
-                });
-
-                if (mappings[symbol] in tokens) {
-                    tokens[mappings[symbol]].push(newToken);
-                } else {
-                    tokens[mappings[symbol]] = [newToken];
-                }
-
-            }
-
-        })
-    })
-
-    return tokens;
-
-}
 
 
 export { 
     getBoundaries, 
 
-    getTokens, 
     mapTokens
  }
