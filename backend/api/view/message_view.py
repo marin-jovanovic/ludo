6bfc3dd7abbc792533b51203076fa2c1ddc4af7d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-18 17:26:04 +0100

    backend api view message_view.py - ref: cleanup

--- backend/api/view/message_view.py
+++ backend/api/view/message_view.py
@@ -3,10 +3,10 @@ import urllib
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from backend.api.view.comm import get_auth_ok_response_template
 from backend.api.model.message import create_message
-
 from backend.api.model.message import get_messages
+from backend.api.view.comm import get_auth_ok_response_template
+
 
 class MessageView(APIView):
 
@@ -21,25 +21,11 @@ class MessageView(APIView):
 
     def post(self, request, game=None):
 
-
-        unquoted_body = urllib.parse.unquote(request.body)
-        body = urllib.parse.parse_qs(unquoted_body)
-
-        try:
-            sender = body["sender"][0]
-            game = body["game"][0]
-            content = body["content"][0]
-        except KeyError:
-            sender = request.data["sender"]
-            game = request.data["game"]
-            content = request.data["content"]
-
-        print(f"{sender=}")
-        print(f"{game=}")
-        print(f"{content=}")
+        sender = request.data["sender"]
+        game = request.data["game"]
+        content = request.data["content"]
 
         response = get_auth_ok_response_template(request)
         response["payload"] = create_message(sender, game, content)
 
         return JsonResponse(response)
-
