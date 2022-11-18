056c85649731ca4693d32e627eda2c09c3dbdd58
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-18 17:26:04 +0100

    backend urls.py - ref: cleanup

--- backend/urls.py
+++ backend/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
 from rest_framework import routers
 
-from backend.api.view.login_view import LoginView
-from backend.api.view.signup_view import SignUpView
-from backend.api.view.logout_view import LogoutView
 from backend.api.startup import startup_configuration
-from backend.api.view.validation_view import ValidationView
-from backend.api.view.lobby_view import LobbyView
-from backend.api.view.settings_view import SettingsView
-from backend.api.view.game_view import GameView
-from backend.api.view.message_view import MessageView
 from backend.api.view.board_view import BoardView
-
+from backend.api.view.game_view import GameView
+from backend.api.view.lobby_view import LobbyView
+from backend.api.view.login_view import LoginView
+from backend.api.view.logout_view import LogoutView
+from backend.api.view.message_view import MessageView
+from backend.api.view.settings_view import SettingsView
+from backend.api.view.signup_view import SignUpView
 
 startup_configuration.print_app_logo()
 api_router = routers.DefaultRouter()
@@ -20,7 +18,6 @@ urlpatterns = [
     path("signup/<str:username>", SignUpView.as_view()),
     path("login/<str:username>", LoginView.as_view()),
     path("logout/<str:username>", LogoutView.as_view()),
-    path("validate/<str:username>", ValidationView.as_view()),
 
     path("lobby/", LobbyView.as_view()),
     path("lobby/<str:name>", LobbyView.as_view()),
@@ -36,5 +33,3 @@ urlpatterns = [
     path("board/<str:name>/<str:resource>", BoardView.as_view()),
 
 ]
-
-
