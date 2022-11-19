9e735c38976687171a8cdf8c1b9a69c636ea8ead
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-19 20:57:59 +0100

    playground marin-jovanovic git.py - wip: separate canvas from bl

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -20,7 +20,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "ref: separate canvas"
+    comment = "wip: separate canvas from bl"
 
     if same_comment:
         if not comment:
