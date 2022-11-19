6ed930835c207c30d7dc7a434fd580fc104f1e1b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-19 18:37:13 +0100

    src views game ludo scripts level.js - ref: separate canvas

--- src/views/game/ludo/scripts/level.js
+++ src/views/game/ludo/scripts/level.js
@@ -1,9 +1,19 @@
-import { getBoundaries,  mapTokens } from "./layers.js"
-import { Boundary } from "./boundary.js"
+import { getBoundaries as getBoardTiles,  mapTokens } from "./layers.js"
+import { BoardTile } from "./board_tile.js"
+import { ContentCreator } from "./content_creator.js";
 
 
-class Level {
-    constructor(map, moves, players, subsFunction=function() {console.log('todo')}) {
+class Level extends ContentCreator {
+    constructor(map, moves, players, subsFunction) {
+        super();
+
+        // todo what is this used for?
+        this.configOneByOne = true;
+
+        // wait for one token to reach destination (stops moving) before other token can be moved on board
+        this.backlog = [];
+        this.useBacklog = false;
+
 
         // todo extract colour to config
         // 
@@ -14,8 +24,9 @@ class Level {
 
         let p = {};
 
+
         for (const [key, value] of Object.entries(players)) {
-            console.log(key, value);
+            // console.log(key, value);
 
             p[key] = {
         
@@ -23,10 +34,11 @@ class Level {
                 username: value.username,
                 tokens: mapTokens({
                     map: map,
-                    Boundary: Boundary,
+                    Boundary: BoardTile,
                     colour: value.colour,
                     subscriber: subsFunction
-                                })
+                                
+                })
             }
         }
 
@@ -47,8 +59,136 @@ class Level {
         this.player1State = moves[0];
 
         // board
-        this.boundaries = getBoundaries({ map: map, Boundary: Boundary });
+        this.boardTiles = getBoardTiles({ map: map, Boundary: BoardTile });
+    
+        // this.start();
     }
+
+    start() {
+        console.log("st")
+        this.notify({command: "animateOnce", level: this, tmp: "test tmp"});
+
+        this.notify({command: "animateLoop", level: this, tmp: "test tmp"});
+
+
+    }
+
+    movePosition({ player, token, jumpCount }) {
+    
+        if (this.useBacklog) {
+            if (this.backlog.length === 0) {
+              this.__movePositionDriver({
+                player: player,
+                token: token,
+                jumpCount: jumpCount,
+              });
+            }
+    
+            this.backlog.push([player, token, jumpCount]);
+          } else {
+            this.__movePositionDriver({
+              player: player,
+              token: token,
+              jumpCount: jumpCount,
+            });
+          }
+    
+}
+
+restartToken({ player, token }) {
+    this.moveTokenToStart({ player: player, token: token });
+}
+
+
+
+    // notify() {
+    //     this.backlog.shift();
+  
+    //     if (this.backlog.length === 0) {
+    //       return;
+    //     }
+  
+    //     let first = this.backlog[0];
+  
+    //     // todo fix, add param for action, or multiple queues
+    //     if (first.length === 2) {
+    //       this.moveTokenToStart({ player: first[0], token: first[1] });
+    //     } else {
+    //       this.movePosition({
+    //         player: first[0],
+    //         token: first[1],
+    //         jumpCount: first[2],
+    //       });
+    //     }
+    //   }
+
+
+
+    _tileToCoordinates(stateBoundaries) {
+        // tile (x,y) to coordinate (pixels x,y ?) 
+        // find center of that pixels posiiton return it
+
+        // todo extract
+        return {
+            x: stateBoundaries.column * BoardTile.width + BoardTile.width / 2,
+            y: stateBoundaries.row * BoardTile.height + BoardTile.height / 2
+        };
+
+    }
+
+    __movePositionDriver({ player, token, jumpCount }) {
+        let t = this.players[player].tokens[token];
+
+        if (this.configOneByOne) {
+
+            for (let i = t.state; i < t.state + jumpCount; i++) {
+
+                if (i in this.player1State) {
+                    let stateBoundaries = this.player1State[i]
+
+                    let destinationPosition = this._tileToCoordinates(stateBoundaries);
+
+                    this.players[player].tokens[token].moveByOne({ destinationPosition: destinationPosition })
+
+                } else {
+                    console.log('integrity error: not in state object')
+                }
+
+            }
+
+        }
+
+        t.state += jumpCount
+
+        let stateBoundaries = this.player1State[t.state]
+
+        let destinationPosition = this._tileToCoordinates(stateBoundaries);
+
+        this.players[player].tokens[token].setDestionationPosition(destinationPosition)
+
+    }
+
+    __restartToken({ player, token }) {
+        this.players[player].tokens[token].restart();
+
+    }
+
+    moveTokenToStart({ player, token }) {
+
+        if (this.useBacklog) {
+            if (this.backlog.length === 0) {
+              this.__restartToken({ player: player, token: token });
+            }
+    
+            this.backlog.push([player, token]);
+        } else {
+            this.__restartToken({ player: player, token: token });
+        }
+
+    }
+
+
+
 }
 
 export { Level }
