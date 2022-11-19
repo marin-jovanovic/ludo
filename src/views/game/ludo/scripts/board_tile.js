b8c7d54fe431b49c8832cf2156060ca07b6f1def
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-11-19 20:57:59 +0100

    src views game ludo scripts board_tile.js - wip: separate canvas from bl

--- src/views/game/ludo/scripts/board_tile.js
+++ src/views/game/ludo/scripts/board_tile.js
@@ -1,12 +1,11 @@
 class BoardTile {
+
     static width = 40;
     static height = 40;
 
     constructor({ position, image }) {
         this.position = position;
-        // this.width = 40;
-        // this.height = 40;
-        this.image = image
+        this.image = image;
     }
 
     draw(canvas) {
