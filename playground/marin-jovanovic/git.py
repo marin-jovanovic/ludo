29bdf606e1a23bf3b18d8b7d05633e35fc8f8e26
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-10 22:48:18 +0100

    playground marin-jovanovic git.py - fix: load static canvas

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -20,7 +20,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: separate canvas from bl"
+    comment = "fix: load static canvas"
 
     if same_comment:
         if not comment:
