f2cee92cdfd5a6aed872ac4df2a44669d92e3d56
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-11 00:48:00 +0100

    src views game ludo scripts ui_board_tile.js - fix: moving tokens

--- src/views/game/ludo/scripts/ui_board_tile.js
+++ src/views/game/ludo/scripts/ui_board_tile.js
@@ -4,33 +4,18 @@ class BoardTile {
     static height = 40;
 
     constructor({ position, image }) {
-
-
         this.position = position;
         this.image = image;
     }
 
     draw(canvas) {
 
-        let image = this.image
+        let image = this.image;
         let position = this.position;
 
         this.image.onload = () => {
-            canvas.drawImage(image, position.x, position.y)
-
+            canvas.drawImage(image, position.x, position.y);
         }
-
-        // this.image.onload = () => {
-        //     canvas.drawImage(this.image, this.position.x, this.position.y)
-
-        // }; 
-
-
-// function drawImageActualSize() {
-//     canvas.drawImage(this.image, this.position.x, this.position.y)
-//   }
-
-        // canvas.drawImage(this.image, this.position.x, this.position.y)
 
     }
 
