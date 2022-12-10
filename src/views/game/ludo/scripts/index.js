466fb7b9b8626f3b8b563bdb3e87c8349f9c98fe
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-11 00:48:00 +0100

    src views game ludo scripts index.js - fix: moving tokens

--- src/views/game/ludo/scripts/index.js
+++ src/views/game/ludo/scripts/index.js
@@ -44,22 +44,49 @@ class Game {
 
         this.bl = new BusinessLogic(config);
 
-        this.bl.currentLevel.subscribe({command: "drawBoard", s: this.ui.staticCanvas.animateOnce});
-        this.bl.currentLevel.subscribe({command: "animateTokens", s: this.ui.reactiveCanvas.animate});
+        this.bl.currentLevel.subscribe({
+            command: "drawBoard", 
+            s: this.ui.staticCanvas.animateOnce
+        });
+        
+        this.bl.currentLevel.subscribe({
+            command: "animateTokens", 
+            s: this.ui.reactiveCanvas.animate
+        });
 
-        // this.ui.staticCanvas.animateOnce();
+        this.bl.currentLevel.subscribe({
+            command: "tokenMoved", 
+            s: this.ui.reactiveCanvas.moveToken
+        });
 
         this.bl.currentLevel.start();
 
     }
 
     movePosition({ player, token, jumpCount }) {
-    
+        /**
+         * @player wants to move @token for @jumpCount positions
+         * 
+         * this can @player only call when it can not be determinated automatically which token should be moved
+         * 
+         * this is the case when there are multiple tokens on the board, or @player rolled 6 and can get one token out of the starting pool or move another @token on the board for 6 positions
+         * 
+         */
+
+
         this.bl.currentLevel.movePosition({player: player, token: token, jumpCount: jumpCount})
         
     }
 
     restartToken({ player, token }) {
+        /**
+         * @player wants to move @token to starting position
+         * this is not something that user can do
+         * 
+         * this can only be done programaticaly => user does not have the option to chose this
+         * 
+         */
+
 
         this.bl.currentLevel.restartToken({ player:player, token:token }) 
 
