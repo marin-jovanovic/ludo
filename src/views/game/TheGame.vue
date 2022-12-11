c3746faa8ecb77bb69dbffb5ac21921d7291df4a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-11 17:15:36 +0100

    src views game TheGame.vue - fix: wip

--- src/views/game/TheGame.vue
+++ src/views/game/TheGame.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <br />
+    user id
+    <input v-model="userid" type="number" placeholder="1" />
+    <br />
+    token id
+    <input v-model="tokenid" type="number" placeholder="1" />
+    <br />
+    jump count
+    <input v-model="jump" type="number" placeholder="1" />
+    <button @click="move">move</button>
+
     <div id="container" class="container">
       <canvas id="staticcanvas" width="600" height="600"></canvas>
       <canvas id="reactivecanvas" width="600" height="600">
@@ -18,12 +29,27 @@ export default {
   data() {
     return {
       game: undefined,
+
+      userid: 0,
+      tokenid: 0,
+      jump: 5,
     };
   },
   async mounted() {
     this.initGame();
   },
   methods: {
+    async move() {
+      console.log(this.userid, this.tokenid, this.jump);
+
+      this.movePosition({
+        player: this.userid,
+        token: this.tokenid,
+        jumpCount: this.jump,
+      });
+
+      //  todo call backend
+    },
     async initGame() {
       let config = ["startPool", "moves", "map", "players"];
       let gameId = this.$route.params.id;
