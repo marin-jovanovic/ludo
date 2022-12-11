72b92fe865e053a45d7960fb36f40f451b0cb5a9
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-11 17:15:36 +0100

    src views gameReplay TheTest.vue - fix: wip

--- src/views/gameReplay/TheTest.vue
+++ src/views/gameReplay/TheTest.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>
-        <button @click="test">test: generate random game</button>
+  <div>
+    <button @click="test">test: generate random game</button>
 
-        <button @click="gostpnti">generate only start till player needs to interact</button>
-    </div>
+    <button @click="gostpnti">
+      generate only start till player needs to interact
+    </button>
+
+
+  </div>
 </template>
     
     
@@ -11,32 +15,34 @@
 import { apiGame } from "@/scripts/api/game";
 
 export default {
-    data() {
-        return {};
+  data() {
+    return {
+
+    };
+  },
+  mounted() {},
+  methods: {
+
+
+    async performAction(action) {
+      let res = await apiGame.actionPerformed(
+        this.gameId,
+        this.username,
+        action
+      );
+
+      if (!(res["auth"]["status"] && res["payload"]["status"])) {
+        console.log("game leave err");
+      }
     },
-    mounted() {
+
+    async gostpnti() {
+      this.performAction("generatestart");
     },
-    methods: {
-        async performAction(action) {
-            let res = await apiGame.actionPerformed(
-                this.gameId,
-                this.username,
-                action
-            );
 
-            if (!(res["auth"]["status"] && res["payload"]["status"])) {
-                console.log("game leave err");
-            }
-
-        },
-
-        async gostpnti() {
-            this.performAction('generatestart');
-        },
-
-        async test() {
-            this.performAction('test');
-        },
+    async test() {
+      this.performAction("test");
     },
+  },
 };
 </script> 
