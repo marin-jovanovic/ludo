efb3f88012ca404b496c0873e8796dbac110c024
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-12 01:51:55 +0100

    src views game ludo scripts ui_comm.js - fix: wip

--- src/views/game/ludo/scripts/ui_comm.js
+++ src/views/game/ludo/scripts/ui_comm.js
@@ -1,4 +1,8 @@
 function remapPosition(i, j, Boundary) {
+    /**
+     * for elements that are placed on tiles (ie. tokens)
+     */
+
     return {
         x: j * Boundary.width + Boundary.width / 2,
         y: i * Boundary.height + Boundary.height / 2
@@ -7,6 +11,10 @@ function remapPosition(i, j, Boundary) {
 }
 
 function remapTile({x,y, Boundary}) {
+    /**
+     * for tiles
+     */
+
     return {
         x: Boundary.width * x,
         y: Boundary.height * y
