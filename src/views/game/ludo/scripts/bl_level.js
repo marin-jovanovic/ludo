7ff44192f970b66f2f258d4721235e101c481488
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-12 01:51:55 +0100

    src views game ludo scripts bl_level.js - fix: wip

--- src/views/game/ludo/scripts/bl_level.js
+++ src/views/game/ludo/scripts/bl_level.js
@@ -6,25 +6,14 @@ import {getConfig} from "./token.js";
 class Level extends ContentCreator {
     constructor({ moves, players, tokens}) {
         super();
-
-        this.config = {
-            // move token one by one
-            // when one token stops moving another can start
-            configOneByOne:  true,
-            // wait for one token to reach destination (stops moving) before other token can be moved on board
-            useBacklog: false,
-        };
-
-  
-        // wait for one token to reach destination (stops moving) before other token can be moved on board
-        this.backlog = [];
-
-        //1 todo extract colour to config
-        //2 sta kad se preklope / pojedu
-        //3 sta kad se preklope useri, (two users at same tile)
         
+        // todo check if this is DTO
         // DTO - data transfer object
-        this.levelState = this.fetchLevelState({players: players, moves: moves, tokens: tokens});
+        this.levelState = this.fetchLevelState({
+            players: players, 
+            moves: moves, 
+            tokens: tokens
+        });
     
     }
 
@@ -85,11 +74,11 @@ class Level extends ContentCreator {
 
     }
 
-    movePosition({ player, token, jumpCount }) {
+    movePosition({ playerId, tokenId, jumpCount }) {
         /**
-            check are performed in backend? 
+            todo check are performed in backend? 
 
-        * can be called by user or automatic (game replay)
+         * can be called by user or automatic (game replay)
          * 
          * perform check if can be done
          * 
@@ -101,51 +90,227 @@ class Level extends ContentCreator {
          */
 
 
-
-
         let isLegalMove =  this.__movePositionDriver({
-            player: player,
-            token: token,
+            playerId: playerId,
+            tokenId: tokenId,
             jumpCount: jumpCount,
-          });
+        });
 
         if (isLegalMove) {
 
+            // move token on ui layer
+
+            let token = this.levelState.players[playerId].tokens[tokenId];
+
+            if (!(token.state in this.levelState.players[playerId].state)) {
+                console.log("err: state not in state dict");
+                // assumption: token was moved to start position
+                // notif: reset was called, no need to launch newDestination notif
+                return;
+            }
+
+            let stateBoundaries = this.levelState.players[playerId].state[token.state];
+
+            // x,y where needs to land
+            let destinationPosition = remapPosition(
+                stateBoundaries.row, 
+                stateBoundaries.column, 
+                BoardTile
+            );
+    
+            token.notify({
+                command: "newDestination",
+                destinationPosition: destinationPosition
+            })
 
             // move token on bl
-            
-            // notify to move token on ui
-
-            console.log("todo notify")
-            // this.notify({
-            //     command: "tokenMoved", 
-            //     player: player,
-            //     token: token,
-            //     jumpCount: jumpCount,
-                    
-            //     // level: this, 
-            // });
+                 
         }
    
     }
 
-    restartToken({ player, token }) {
-        this.levelState.players[player].tokens[token].restart();
+    restartToken({ playerId, tokenId }) {
+        this.levelState.players[playerId].tokens[tokenId].restart();
     }
 
-    getObjectMaxKey(obj) {
-        return Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
+    isNotJumpingOverRestricted = ({jumpCount, player, tokenId}) => {
+        /**
+         * return true if can perform this jump
+         * return false else
+         */
+        
+
+        // we are viewing this in relative perspective
+
+        let t = this.levelState.players[player].tokens[tokenId];
+        let restrictedJumpingOver = [53, 52, 51, 50, 49, 48]; 
+
+
+        if (!( restrictedJumpingOver.includes(
+            (t.state + jumpCount)
+        ))) {
+            return true
+        }
+
+        let occupiedSpaces = [];
+
+        for (const [t, tMeta] of Object.entries(this.levelState.players[player].tokens)) {
+
+            if (Number(t) !== Number(tokenId)) {
+
+                if ( restrictedJumpingOver.includes(tMeta.state)) {
+                    occupiedSpaces.push(tMeta.state);
+                }
+            }
+        }
+
+       let lowest = Math.min(...occupiedSpaces);
+
+       return t.state + jumpCount < lowest
+
     }
 
-    __movePositionDriver({ player, token, jumpCount }) {
+    isGameWon = ({playerId}) => {
 
-        if (!(player in this.levelState.players)) {
-            console.log("err: unknown player", player);
+
+        let restrictedJumpingOver = [53, 52, 51, 50]; 
+        let occupiedSpaces = [];
+
+        for (
+            const tMeta of Object.values(this.levelState.players[playerId].tokens)
+        ) {
+
+                if ( restrictedJumpingOver.includes(tMeta.state)) {
+                    occupiedSpaces.push(tMeta.state);
+                }
+        }
+   
+        return occupiedSpaces.length === restrictedJumpingOver.length;
+
+    }
+
+    checkEating = ({playerId, tokenId}) => {
+
+        console.log();
+        // absolute view
+        
+        let occupiedSpaces = {};
+        console.log(playerId, tokenId, occupiedSpaces)
+
+        let token = this.levelState.players[playerId].tokens[tokenId];
+        
+        let stateOfInteres = token.absoluteState;
+
+        for (const [playerId, p] of Object.entries(this.levelState.players)) {
+
+            Object.values(p.tokens).forEach(t => {
+
+                if (t !== token) {
+
+                    let k = playerId;
+
+                    if (t.absoluteState === stateOfInteres) {
+                 
+                        if (k in occupiedSpaces) {
+                            occupiedSpaces[k].push(t);
+                        } else {
+                            occupiedSpaces[k] = [t];
+                        }
+   
+                    }
+
+                }
+            });
+
+        }
+
+        // Object.values(this.levelState.players).forEach(p => {
+            
+    
+        // });
+
+        console.log("tokens at this place", occupiedSpaces)
+
+
+        console.log("player id", playerId)
+
+        /*
+            case 1:
+                this user has any tokens here
+            case 2:
+                1 other user has 1 token here
+                that token is removed
+                this token stays
+            case 3:
+                1 other user has more than 1 token here
+                block is formed
+                this token is removed
+            case 4:
+                2 other users have tokens here
+                if first one formed a block then all tokens from second player are removed => goto case 3
+
+                if first one has one token and second player moves his token to this piece then the first token is remove => goto case 2
+
+            conclusion: 
+                if my tokens are here 
+                    do nothing
+                if other user token is here 
+                    if 1
+                        remove that token
+                    if more then 1
+                        remove this token
+        */
+
+        if (occupiedSpaces.length === 1) {
+            console.log("only my tokens are here")
+        } else {
+            for (const [owner, tokens] of Object.entries(occupiedSpaces)) {
+                console.log(owner, playerId, typeof(owner), typeof(playerId));
+
+                console.log(Number(owner), Number(owner) !== playerId)
+
+                if (Number(owner) !== playerId) {
+                    if (tokens.length === 1) {
+                        console.log("1 enemy token, perform eating");
+
+                        tokens.forEach(t => {
+                            t.restart();
+                        });
+
+
+                    } else if (tokens.length >= 1) {
+                        console.log("enemy tokens forming a block, this token goes to start position")
+
+                        token.restart();
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    moveTokenFromStartingPoolToLivePool({token, playerId}) {
+        // todo dehardcode absolute state
+
+        token.pool = getConfig()["pool"]["live"];
+        token.state = 0;
+        token.absoluteState = 12 * playerId;
+
+    }
+
+    __movePositionDriver({ playerId, tokenId, jumpCount }) {
+        /**
+         * move token on business layer
+         */
+
+        if (!(playerId in this.levelState.players)) {
+            console.log("err: unknown player", playerId);
             return;
         }
 
-        if (!(token in this.levelState.players[player].tokens)) {
-            console.log("err: unknown token", token);
+        if (!(tokenId in this.levelState.players[playerId].tokens)) {
+            console.log("err: unknown token", tokenId);
             return;
         }
 
@@ -154,74 +319,78 @@ class Level extends ContentCreator {
         //     return;
         // }
 
-        // current token 
-        let t = this.levelState.players[player].tokens[token];
+        let token = this.levelState.players[playerId].tokens[tokenId];
 
-        switch (t.pool) {
+        switch (token.pool) {
             case getConfig()["pool"]["start"]:
-                t.pool = getConfig()["pool"]["live"];
-                t.state = 0;
+                this.moveTokenFromStartingPoolToLivePool({
+                    token: token,
+                    playerId: playerId
+                });
                 break;
             
             case getConfig()["pool"]["live"]:
 
                 // check if can move so much 
-
-                console.log(
-                    t.state,
-                    jumpCount,
-                    this.levelState.players[player].state
-                )
-
+             
                 if (
-                    t.state + jumpCount
-                    in this.levelState.players[player].state
+                    token.state + jumpCount
+                    in this.levelState.players[playerId].state
                 ) {
+                    // state exists
 
-                    console.log("can jump");
+                    if (this.isNotJumpingOverRestricted({
+                        jumpCount: jumpCount, 
+                        player: playerId, 
+                        tokenId: tokenId
+                    })) {
 
-                                    // bl
-                    t.move({
-                        count: jumpCount, 
-                    });
+                        // bl
+                        token.move({
+                            count: jumpCount, 
+                        });
+
+                    }else {
+                        console.log("can not jump over other token in restricted jumping area")
+                    }
 
                 } else {
-                    console.log("cant  jump")
+                    console.log("ret false")
+                    // return false;
                 }
-
 
                 break;
 
             case getConfig()["pool"]["done"]:
 
+                // todo add logic that add this token to done pool
                 console.log("at destination position")
 
                 break;
     
             default:
+
+                console.log("err: unknown")
                 break
                 
         }
           
-        if (!(t.state in this.levelState.players[player].state)) {
-            console.log("not there", t.state)
+        if (!(token.state in this.levelState.players[playerId].state)) {
+            console.log("err not there", token.state)
             return
         }
 
-        let stateBoundaries = this.levelState.players[player].state[t.state]
 
-        // x,y where needs to land
-        let destinationPosition = remapPosition(
-            stateBoundaries.row, 
-            stateBoundaries.column, 
-            BoardTile
-        );
+        if (this.isGameWon({playerId: playerId})) {
+            console.log("todo game won");
+        } 
 
-        this.levelState.players[player].tokens[token].notify({
-            command: "newDestination",
-            destinationPosition: destinationPosition
-        })
-                
+        this.checkEating({
+            playerId: playerId,
+            tokenId: tokenId
+        });
+
+
         return true;
     }
 
