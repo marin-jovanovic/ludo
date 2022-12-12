3260038be4f05f386580f8a1f45d98d6d118198f
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-12 01:51:55 +0100

    src views game ludo scripts content_creator.js - fix: wip

--- src/views/game/ludo/scripts/content_creator.js
+++ src/views/game/ludo/scripts/content_creator.js
@@ -7,18 +7,13 @@ class ContentCreator {
      */
 
     constructor() {
-        // console.log("created")
         this.subscribers = {};
     }
 
     subscribe({command, s}) {
 
-        // console.log("subscribe", command, s)
-
         if (command in this.subscribers) {
-
             this.subscribers[command].add(s);
-
         } else {
             this.subscribers[command] = new Set();
             this.subscribers[command].add(s);
@@ -32,28 +27,10 @@ class ContentCreator {
 
     notify({command, ...args}) {
 
-        // console.log("notif", command, args)
-
         if (!(command in this.subscribers)) {
             console.log("no subscribers for this command");
             return;
         }
-
-        // if (this.subscribers[command]) {
-
-        //     console.log("can call")
-        // }else {
-        //     console.log("no function to call")
-        // }
-
-        // if (i in this.subscribers[command]) {
-
-        // }
-
-        // console.log(command)
-        // console.log(
-        //     this.subscribers[command]    
-        // )
 
         this.subscribers[command].forEach((i) => {
             i(args);
