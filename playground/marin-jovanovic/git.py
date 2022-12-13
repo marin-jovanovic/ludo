f1ebe0b98efe857049d5d17035098766a8b303c5
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-13 01:28:29 +0100

    playground marin-jovanovic git.py - wip: board builder

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -20,7 +20,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "fix: wip"
+    comment = "wip: board builder"
 
     if same_comment:
         if not comment:
