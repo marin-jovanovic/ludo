148b9c0a9d8c80ec74e5241d120e565a3f69960d
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-14 20:29:18 +0100

    src views gameCreate board_builder scripts index.js - fix: token paths

--- src/views/gameCreate/board_builder/scripts/index.js
+++ src/views/gameCreate/board_builder/scripts/index.js
@@ -1,4 +1,10 @@
-
+/**
+ * todo
+ * 
+ * scrolling?
+ * layers
+ * export
+ */
 
 // function run () => {
 //     window.onload = function () {
@@ -30,6 +36,7 @@ class BoardBuilder {
 
         }
 
+        this.redraw();
 
     }
 
@@ -44,14 +51,140 @@ class BoardBuilder {
 
         }
 
+        this.redraw();
     }
 
     selectLayer = ({layerId}) => {
         this.selectedLayer = layerId;
+
     }
 
     updateTile = ({row, column}) => {
         this.board[row][column] = String(this.selectLayer);
+
+    }
+
+    // draw a single rect
+    rect = (x, y, w, h) => {
+        this.ctx.beginPath();
+        this.ctx.rect(x, y, w, h);
+        this.ctx.closePath();
+        this.ctx.fill();
+    }
+
+    // clear the canvas
+    clear = () => {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    // redraw the scene
+    draw = () => {
+        this.clear();
+        this.ctx.fillStyle = "#FAF7F8";
+        this.rect(0, 0, this.canvas.width, this.canvas.height);
+        // redraw each rect in the rects[] array
+        for (var i = 0; i < this.rects.length; i++) {
+            var r = this.rects[i];
+            this.ctx.fillStyle = r.fill;
+            this.rect(r.x, r.y, r.width, r.height);
+        }
+    }
+
+    // handle mousedown events
+    myDown = (e) => {
+
+        // tell the browser we're handling this mouse event
+        e.preventDefault();
+        e.stopPropagation();
+
+        // get the current mouse position
+        var mx = parseInt(e.clientX - this.offsetX);
+        var my = parseInt(e.clientY - this.offsetY);
+
+        // test each rect to see if mouse is inside
+        this.dragok = false;
+        for (var i = 0; i < this.rects.length; i++) {
+            var r = this.rects[i];
+            if (mx > r.x && mx < r.x + r.width && my > r.y && my < r.y + r.height) {
+                // if yes, set that rects isDragging=true
+                this.dragok = true;
+                r.isDragging = true;
+            }
+        }
+        // save the current mouse position
+        this.startX = mx;
+        this.startY = my;
+    }
+
+
+    // handle mouseup events
+    myUp = (e) => {  
+        // tell the browser we're handling this mouse event
+        e.preventDefault();
+        e.stopPropagation();
+
+        // clear all the dragging flags
+        this.dragok = false;
+        for (var i = 0; i < this.rects.length; i++) {
+            this.rects[i].isDragging = false;
+        }
+    }
+
+
+    // handle mouse moves
+    myMove = (e) => {
+        if (this.dragok) {
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            var mx = parseInt(e.clientX - this.offsetX);
+            var my = parseInt(e.clientY - this.offsetY);
+
+
+            var dx = mx - this.startX;
+            var dy = my - this.startY;
+
+            // move each rect that isDragging 
+            // by the distance the mouse has moved
+            // since the last mousemove
+            for (var i = 0; i < this.rects.length; i++) {
+                var r = this.rects[i];
+                if (r.isDragging) {
+                    r.x += dx;
+                    r.y += dy;
+                }
+            }
+
+            // redraw the scene with the new rect positions
+            this.draw();
+
+            // reset the starting mouse position for the next mousemove
+            this.startX = mx;
+            this.startY = my;
+
+        }
+    }
+
+    redraw = () => {
+      
+        let height = Object.keys(this.board).length;
+
+        let width = Object.keys(this.board[0]).length;
+
+        console.log(width)
+
+        for (let w = 0; w < width; w++) {
+            for (let h = 0; h < height; h++) {
+
+                this.ctx.fillStyle = ["red", "blue", "cyan", "yellow"][this.board[h][w]]
+
+                // fillRect(x, y, width, height)
+                this.ctx.fillRect(w * this.wSize, h * this.hSize, this.wSize, this.hSize);
+            
+            }
+        }
+
     }
 
     constructor({canvasElement}) {
@@ -64,7 +197,7 @@ class BoardBuilder {
         this.canvas.width = innerWidth;
         this.canvas.height = innerHeight - 500;
 
-        const ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext('2d');
 
         this.board = {
             0: {0: 0, 1: 1},
@@ -81,270 +214,104 @@ class BoardBuilder {
         console.table(this.board)
 
 
-        let wSize = 100;
-        let hSize = 100;
+        this.wSize = 100;
+        this.hSize = 100;
       
-        let height = Object.keys(this.board).length;
+        this.redraw();
 
-        let width = Object.keys(this.board[0]).length;
+        /**
+         * drag and drop part
+         */
 
 
-        for (let w = 0; w < width; w++) {
-            for (let h = 0; h < height; h++) {
+        // // get canvas related references
+        // var BB = this.canvas.getBoundingClientRect();
+        // this.offsetX = BB.left;
+        // this.offsetY = BB.top;
+        // // var WIDTH = this.canvas.width;
+        // // var HEIGHT = this.canvas.height;
 
-                ctx.fillStyle = ["red", "blue", "cyan", "yellow"][this.board[h][w]]
+        // // drag related variables
+        // this.dragok = false;
+        // this.startX;
+        // this.startY;
 
-                // fillRect(x, y, width, height)
-                ctx.fillRect(w * wSize, h * hSize, wSize, hSize);
-            
-            }
-        }
+        // // an array of objects that define different rectangles
+        // this.rects = [];
+        // this.rects.push({
+        //     x: 75 - 15,
+        //     y: 50 - 15,
+        //     width: 30,
+        //     height: 30,
+        //     fill: "#444444",
+        //     isDragging: false
+        // });
+        // this.rects.push({
+        //     x: 75 - 25,
+        //     y: 50 - 25,
+        //     width: 30,
+        //     height: 30,
+        //     fill: "#ff550d",
+        //     isDragging: false
+        // });
+        // this.rects.push({
+        //     x: 75 - 35,
+        //     y: 50 - 35,
+        //     width: 30,
+        //     height: 30,
+        //     fill: "#800080",
+        //     isDragging: false
+        // });
+        // this.rects.push({
+        //     x: 75 - 45,
+        //     y: 50 - 45,
+        //     width: 30,
+        //     height: 30,
+        //     fill: "#0c64e8",
+        //     isDragging: false
+        // });
+
+        // // listen for mouse events
+        // this.canvas.onmousedown = this.myDown;
+        // this.canvas.onmouseup = this.myUp;
+        // this.canvas.onmousemove = this.myMove;
+
+        // // call to draw the scene
+        // this.draw();
+
+        /**
+         * 
+         */
+
+
+       
         
-        var elem = this.canvas;
+        /**
+         * this will colour curr block to black
+         */
 
-        let elemLeft = elem.offsetLeft;
-        let elemTop = elem.offsetTop;
-        // let elements = [];
+        let elemLeft = this.canvas.offsetLeft;
+        let elemTop = this.canvas.offsetTop;
 
-        // Add event listener for `click` events.
-        elem.addEventListener('click', function(event) {
+        this.canvas.addEventListener('click', (event) => {
             var x = event.pageX - elemLeft,
                 y = event.pageY - elemTop;
             // console.log(x, y);
 
-            let xBlock = Math.floor(x / wSize);
-            let yBlock = Math.floor(y / hSize);
+            let xBlock = Math.floor(x / this.wSize);
+            let yBlock = Math.floor(y / this.hSize);
 
-            console.log("clicked", xBlock, yBlock)
+            console.log("clicked", xBlock, yBlock);
 
-
-
-            // for (let w = 0; w < width; w++) {
-            //     for (let h = 0; h < height; h++) {
-
-            //         // let element = 
-
-            //         console.log()
-
-            //         // if (
-            //         //     y > element.top && 
-            //         //     y < element.top + element.height && 
-            //         //     x > element.left && 
-            //         //     x < element.left + element.width
-            //         // ) {
-            //         //     console.log('clicked an element', w, h);
-            //         // }
-        
-                
-            //     }
-            // }
-    
-
-            // elements.forEach(function(element) {
-            //     if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
-            //         alert('clicked an element');
-            //     }
-            // });
+            this.board[yBlock][xBlock] = -1;
+            this.ctx.fillStyle = "black";
+            this.ctx.fillRect(xBlock * this.wSize, yBlock * this.hSize, this.wSize, this.hSize);
+            
 
         }, false);
 
     
     }
-
-    // constructor({canvasElement}) {
-
-    //     // todo
-    //     this.selectedLayer = 1;
-
-    //     this.canvas = canvasElement;
-
-    //     this.canvas.width = innerWidth;
-    //     this.canvas.height = innerHeight - 500;
-
-    //     const ctx = this.canvas.getContext('2d');
-
-    //     this.board = {
-    //         0: {0: 0, 1: 1},
-    //         1: {0: 2, 1: 3},
-            
-    //     };
-
-    //     this.defaultValue = 0;
-        
-    //     this.addRow();
-
-    //     this.addColumn();
-
-    //     console.table(this.board)
-
-    //     var tilesize = 30;
-
-    //     // var map = {
-    //     //     0: {0: 0, 1: 1},
-    //     //     1: {0: 2, 1: 3},
-    //     //     2: {0: 0, 1: 1},
-    //     // }
-      
-    //     let height = Object.keys(this.board).length;
-
-    //     let width = Object.keys(this.board[0]).length;
-
-
-    //     for (let w = 0; w < width; w++) {
-    //         for (let h = 0; h < height; h++) {
-
-    //             ctx.fillStyle = ["red", "blue", "cyan", "yellow"][this.board[h][w]]
-
-    //             ctx.fillRect(w * tilesize, h * tilesize, tilesize, tilesize)
-            
-    //         }
-    //     }
-        
-    //     var elem = this.canvas;
-
-    //     let elemLeft = elem.offsetLeft;
-    //     let elemTop = elem.offsetTop;
-    //     // let elements = [];
-
-    //     // Add event listener for `click` events.
-    //     elem.addEventListener('click', function(event) {
-    //         var x = event.pageX - elemLeft,
-    //             y = event.pageY - elemTop;
-    //         console.log(x, y);
-
-    //         for (let w = 0; w < width; w++) {
-    //             for (let h = 0; h < height; h++) {
-
-    //                 // let element = 
-
-    //                 console.log()
-
-    //                 // if (
-    //                 //     y > element.top && 
-    //                 //     y < element.top + element.height && 
-    //                 //     x > element.left && 
-    //                 //     x < element.left + element.width
-    //                 // ) {
-    //                 //     console.log('clicked an element', w, h);
-    //                 // }
-        
-                
-    //             }
-    //         }
-    
-
-    //         // elements.forEach(function(element) {
-    //         //     if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
-    //         //         alert('clicked an element');
-    //         //     }
-    //         // });
-
-    //     }, false);
-
-
-
-
-    //     // this.c = 0;
-
-    //     // this.tableCreate(15, 15);
-    
-    //     // this.activateExportButton();
-    
-    // }
-
-//     tableCreate = ({row, col}) => {
-//         const body = document.body;
-//         let tbl = document.createElement('table');
-
-//         for (let i = 0; i < row; i++) {
-//             const tr = tbl.insertRow();
-//             for (let j = 0; j < col; j++) {
-
-//                 const td = tr.insertCell();
-
-//                 td.style.border = '1px solid black';
-//                 td.style.backgroundColor = 'white'
-//                 td.style.width = '20px'
-//                 td.style.height = '20px'
-
-//                 td.addEventListener('click', () => {
-//                     if (td.innerText) {
-//                         console.log('delete')
-//                         td.innerText = ''
-//                         this.c--;
-//                     } else {
-//                         td.innerText = this.c;
-//                         this.c++;
-//                     }
-//                 })
-//             }
-//         }
-//         body.appendChild(tbl);
-//     }
-
-//     activateExportButton = () => {
-//     let exportButton = document.querySelector('#exportButton');
-
-//     exportButton.addEventListener('click', () => {
-
-//         let table = document.querySelector('table');
-
-//         let exportTable = [];
-
-//         let logSelected = {};
-
-//         let row;
-//         for (let i = 0; row = table.rows[i]; i++) {
-
-//             exportTable[i] = [];
-
-//             let col;
-//             for (var j = 0; col = row.cells[j]; j++) {
-//                 if (col.innerText) {
-//                     logSelected[col.innerText] = { row: i, column: j };
-//                 } 
-//             }
-//         }
-
-//         console.table(logSelected)
-//         console.log(logSelected)
-
-//     })
-
-// }
-
-//     buildLayer = () => {
-
-//     let table = document.querySelector('table');
-
-//     for (var i = 0, row; row = table.rows[i]; i++) {
-
-
-//         for (var j = 0, col; col = row.cells[j]; j++) {
-//             // console.log(col)
-//             col.addEventListener('click', i => {
-//                 // console.log(i.target.style)
-//                 console.log(i.target)
-//                 // console.
-//                 i.target.backgroundColor = 'black'
-//                 // if (col.style.backgroundColor === 'black') {
-//                 //     col.style.backgroundColor = 'white'
-//                 // } else {
-//                 //     col.style.backgroundColor = 'black'
-//                 // }
-//             })
-
-
-//             // if (col.style.backgroundColor === 'black') {
-//             //     exportTable[i][j] = selected
-//             //     logSelected[logSelectedIndex] = { row: i, column: j };
-//             //     logSelectedIndex++;
-//             // } else {
-//             //     exportTable[i][j] = unselected
-
-//             // }
-//         }
-//     }
 
 }
 
