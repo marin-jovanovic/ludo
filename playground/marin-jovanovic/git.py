7f2842e37d26b4c395dd8996b62bf6bbdf022b5b
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-14 20:29:18 +0100

    playground marin-jovanovic git.py - fix: token paths

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -20,7 +20,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "wip: board builder"
+    comment = "fix: token paths"
 
     if same_comment:
         if not comment:
