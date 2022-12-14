dd17c05d3296b6fe37038a47e31770a4735f5e10
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-15 00:43:35 +0100

    src views game ludo scripts ui_token.js - impl: token move field by field

--- src/views/game/ludo/scripts/ui_token.js
+++ src/views/game/ludo/scripts/ui_token.js
@@ -1,5 +1,6 @@
 import { ContentCreator } from "./content_creator.js";
-
+import { remapPosition } from "./ui_comm.js";
+import { BoardTile } from "./ui_board_tile.js";
 
 /**
  * ===>>>
@@ -32,6 +33,7 @@ class UiToken extends ContentCreator {
         this.position = position;
         // todo determine (if game mode all_at_same_place then you know) else one by one
         // what if second is faster then this first one, still undetermined, this must update depending which ends first 
+        
         this.destinationPosition = position;
 
         this.radius = 8
@@ -56,7 +58,39 @@ class UiToken extends ContentCreator {
 
         this.choice_oneByOne_pythagoras = true;
 
+        // this.previousPosition = undefined;
+
     }
+
+    isBacklogEmpty = () => {
+        return this.backlog.length === 0;
+    }
+
+    _getDestinationByOne = () => {
+        if (this.isBacklogEmpty()) {
+            console.log('integrity error');
+            return
+        }
+
+        return this.backlog[0];
+    }
+
+    _sleep = () => {
+        if (
+            this.backlogSleepFrameDec === this.backlogSleepFrameCount
+        ) {
+
+            this.startSleeping = false;
+            this.backlogSleepFrameDec = 0;
+
+        } else {
+
+            this.backlogSleepFrameDec += 1;
+
+        }
+
+    }
+
 
     restart = () => {
         /**
@@ -71,8 +105,67 @@ class UiToken extends ContentCreator {
  
     }
 
-    setDestionationPosition = ({destinationPosition}) => {
+    getNextDestination = () => {
+        // console.lo
+
+        // let top = stack[stack.length-1];
+
+        let first = this.backlog[0];
+        // console.log(first);
+        return first;
+
+    }
+
+    setDestionationPosition = ({destinationPosition, diff}) => {
+    // setDestionationPosition = ({diff}) => {
+
+        // console.log("diff", diff)/
+
+        if (this.backlog.length === 0) {
+            this.backlog = diff;
+        } else {
+            console.log("err backlog load");
+        }
+
+        // console.log(this.backlog)
+
+        // this.backlog.forEach(i => {
+        //     console.log(i.row, i.column)
+        // });
+
+        this.getNextDestination();
+
+
+
+        if (! this.isBacklogEmpty()) {
+            // this._moveByOneToDestination();
+                
+                    let destination = this.getNextDestination();
+    
+            console.log(BoardTile)
+
+            console.log(
+                {
+                    i: destination.row,
+                    j: destination.column,
+                    Boundary: BoardTile
+                }
+            )
+
+        let des = remapPosition({
+            i: destination.row,
+            j: destination.column,
+            Boundary: BoardTile
+        })
+
+        console.log(des)
+        }
+
+
+        // nex
+
         this.destinationPosition = destinationPosition;
+
     }
 
     // moveByOne = ({ destinationPosition }) => {
@@ -81,8 +174,18 @@ class UiToken extends ContentCreator {
     //     this.setDestionationPosition(destinationPosition)
     // }
 
+    _moveByOneToDestination = () => {
 
-    _moveByOneToDestination = (des) => {
+    // _moveByOneToDestination = (des) => {
+        let destination = this.getNextDestination();
+    
+        let des = remapPosition({
+            i: destination.row,
+            j: destination.column,
+            Boundary: BoardTile
+        })
+
+
         let atNextDestination = true;
 
         ['x', 'y'].forEach(i => {
@@ -103,21 +206,79 @@ class UiToken extends ContentCreator {
     }
 
     draw = (c) => {
+             c.beginPath()
+             c.arc(
+                 this.position.x,
+                 this.position.y,
+                 this.radius,
+                 0,
+                 Math.PI * 2,
+             );
+     
+             let wasAtDestination = this.isBacklogEmpty();
+     
+             if (!wasAtDestination) {
+                 this._moveByOneToDestination();
+     
+             }
+     
+             if (!wasAtDestination) {
+     
+                 if (this.configOneByOne) {
+                     if (this.configSleep && this.startSleeping) {
+                         this._sleep();
+                     } else {
+                         
+                         let atNextDestination = this._moveByOneToDestination(
+                             this._getDestinationByOne()
+                         );
+     
+                         if (atNextDestination) {
+                             this.backlog.shift();
+     
+                             this.startSleeping = true;
+                         }
+     
+                     }
+                 } else {
+                     this._moveByOneToDestination(this.destinationPosition);
+                 }
+             }
+     
+             let isAtDestination = this.isBacklogEmpty();
+     
+             if (!wasAtDestination && isAtDestination) {
+                 // check if after redraw the token will be at the destination
+                 // if the token is at the destination then notify
+                 
+                 console.log("notify")
+                //  this.notify({command: "token stopped"});
+             }
+     
+             c.fillStyle = this.colour
+             c.fill()
+             c.closePath()
         
-        c.beginPath()
-        c.arc(
-            this.position.x,
-            this.position.y,
-            this.radius,
-            0,
-            Math.PI * 2,
-        );
+        // c.beginPath()
+        // c.arc(
+        //     this.position.x,
+        //     this.position.y,
+        //     this.radius,
+        //     0,
+        //     Math.PI * 2,
+        // );
 
-        this._moveByOneToDestination(this.destinationPosition);
+        // if (! this.isBacklogEmpty()) {
+        //     this._moveByOneToDestination();
+                
+        // }
 
-        c.fillStyle = this.colour
-        c.fill()
-        c.closePath()
+
+        // this._moveByOneToDestination(this.destinationPosition);
+
+        // c.fillStyle = this.colour
+        // c.fill()
+        // c.closePath()
     }
 
 }
