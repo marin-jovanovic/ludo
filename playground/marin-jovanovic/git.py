32ba5465eefb46aacb04783c6622fc4fdba4aad2
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-15 02:20:55 +0100

    playground marin-jovanovic git.py - impl: token move field by field notifiers

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -20,7 +20,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "impl: token move field by field"
+    comment = "impl: token move field by field notifiers"
 
     if same_comment:
         if not comment:
