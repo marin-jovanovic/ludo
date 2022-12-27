3aa29ba1201d8e6b5efa55c3eb3ce1f33f635947
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-27 23:44:16 +0100

    playground marin-jovanovic git.py - impl: #15

--- playground/marin-jovanovic/git.py
+++ playground/marin-jovanovic/git.py
@@ -20,7 +20,7 @@ def main():
     comment = "impl: create / update portfolio"
     comment = 'ref: rewrite db scheme'
 
-    comment = "fix: #2"
+    comment = "impl: #15"
 
     if same_comment:
         if not comment:
