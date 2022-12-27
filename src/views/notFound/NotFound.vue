ce98a01c3499d338b103d5e6c2f18fe8b2ac8e64
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-12-27 23:44:16 +0100

    src views notFound  - impl: #15

---
+++ src/views/notFound/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <BasePublicTemplate>
+    Page not found
+    <TheLinks></TheLinks>
+  </BasePublicTemplate>
+</template>
+
+<script>
+import BasePublicTemplate from "@/components/BasePublicTemplate.vue";
+import TheLinks from "@/components/TheLinks.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: { BasePublicTemplate, TheLinks },
+};
+</script> 
