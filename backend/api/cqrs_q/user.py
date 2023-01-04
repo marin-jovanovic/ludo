ce35f6450586ea0b2f16161abe3b9b01f099ba4b
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-01-04 21:15:31 +0100

    backend api cqrs_q user.py - wip: models cleanup

--- backend/api/cqrs_q/user.py
+++ backend/api/cqrs_q/user.py
@@ -3,18 +3,26 @@ import hashlib
 
 import bcrypt
 
-from backend.api.model.player import get_user_model
+from backend.api.model.user import get_user_model
+
+
+def get_user_object(username):
+    return get_user_model().objects.get(username=username)
 
 
 def is_username_in_db(username):
     return get_user_model().objects.filter(username=username).exists()
 
 
+def get_access_token(username):
+    return get_user_model().objects.get(username=username).access_token
+
+
 def is_access_token_correct(username, access_token):
     if not is_username_in_db(username):
         return {"status": False}
 
-    r = get_user_model().objects.get(username=username).access_token == access_token
+    r = get_access_token(username) == access_token
 
     return {'status': r}
 
@@ -27,21 +35,23 @@ def is_authenticated(username, password):
     pass_transformed = base64.b64encode(
         hashlib.sha256(pass_long).digest())
 
-    pwd_from_db = get_user_model().objects.get(username=username).password_hash.encode(
+    pwd_from_db = get_user_model().objects.get(
+        username=username).password_hash.encode(
         'utf-8')
     return bcrypt.checkpw(pass_transformed, pwd_from_db)
 
 
 def get_logged_users():
     return list(
-        get_user_model().objects.filter(access_token__isnull=False).values("username"))
+        get_user_model().objects.filter(access_token__isnull=False).values(
+            "username"))
 
 
-def get_users_in_level(game_id):
+def get_users_in_level(level_id):
     return {
         "status": True,
         "payload": list(get_user_model().objects.filter(
-            currently_playing__name=game_id,
+            currently_playing__id=level_id,
             currently_playing__is_active=True
         ))
     }
@@ -51,9 +61,10 @@ def get_user(username):
     try:
         return {
             "status": True,
-            "payload": get_user_model().objects.get(username=username)
+            "payload": get_user_object(username)
         }
     except get_user_model().DoesNotExist as e:
+        print("get user err")
         return {
             "status": False,
             "debug": "user with this username does not exist"
