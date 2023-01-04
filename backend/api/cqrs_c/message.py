260599e045e3b76e7a4004a1292eab993b93a93b
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-01-04 21:15:31 +0100

    backend api cqrs_c message.py - wip: models cleanup

--- backend/api/cqrs_c/message.py
+++ backend/api/cqrs_c/message.py
@@ -1,6 +1,6 @@
 import json
 
-from backend.api.cqrs_q.level import __check_game_name_exists, __get_game
+from backend.api.cqrs_q.level import level_get_filter_id
 from backend.api.cqrs_q.user import get_user
 from backend.api.model.message import Message, message_notifier
 
@@ -17,17 +17,15 @@ def create_message(sender, game, content):
         print(r)
         user_o = r["payload"]
 
-    r = __check_game_name_exists(game)
-    if not r["payload"]:
-        return {"status": False, "payload": "gamen ot exist"}
+    # r = __check_game_name_exists(game)
+    # if not r["payload"]:
+    #     return {"status": False, "_": "__check_game_name_exists"}
 
-    r = __get_game(game)
-    if not r["payload"]:
-        # print(r)
-        # print("r payload prsnet")
-        return {"status": False, "payload": "game not exiswt"}
-    else:
-        game_o = r["payload"]
+    r = level_get_filter_id(game)
+    if not r["status"]:
+        return {"status": False, "_": "__get_game"}
+
+    game_o = r["payload"]
 
     g = Message(
         game=game_o,
