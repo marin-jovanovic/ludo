004765ff564b00c7c440c058782f030785f9fc59
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-01-04 21:15:31 +0100

    backend urls.py - wip: models cleanup

--- backend/urls.py
+++ backend/urls.py
@@ -2,20 +2,18 @@ from django.urls import path
 from rest_framework import routers
 
 from backend.api.startup import startup_configuration
-
-from backend.api.view.level_view import LevelView
+# from backend.api.view.game_view import GameView
+from backend.api.view.acceptance_log_view import AcceptanceLogView
+from backend.api.view.board_view import BoardView
+from backend.api.view.delete_profile_view import DeleteProfileView
 from backend.api.view.level_log_view import LevelLogView
-
+from backend.api.view.level_view import LevelView
 from backend.api.view.login_view import LoginView
 from backend.api.view.logout_view import LogoutView
 from backend.api.view.message_view import MessageView
 from backend.api.view.settings_view import SettingsView
 from backend.api.view.signup_view import SignUpView
-from backend.api.view.delete_profile_view import DeleteProfileView
 from backend.api.view.user_view import UserView
-from backend.api.view.board_view import BoardView
-# from backend.api.view.game_view import GameView
-from backend.api.view.acceptance_log_view import AcceptanceLogView
 
 startup_configuration.print_app_logo()
 api_router = routers.DefaultRouter()
@@ -28,21 +26,25 @@ urlpatterns = [
 
     # CRUD meta game
     path("level/", LevelView.as_view()),
-    path("level/<str:name>", LevelView.as_view()),
+
+    # todo actually this is level_name for post method
+    path("level/<str:level_id>", LevelView.as_view()),
+    # path("level/<str:level_id>", LevelView.as_view()),
     path("level/<str:level_id>/log", LevelLogView.as_view()),
 
     path("message/", MessageView.as_view()),
-    path("message/<str:game>", MessageView.as_view()),
+    path("message/<str:level_id>", MessageView.as_view()),
 
     path("settings/", SettingsView.as_view()),
 
     path("user/<str:user_id>", UserView.as_view()),
 
     path("level/<str:level_id>/acceptanceLog", AcceptanceLogView.as_view()),
-    path("level/<str:level_id>/acceptanceLog/<str:entry_id>", AcceptanceLogView.as_view()),
+    path("level/<str:level_id>/acceptanceLog/<str:entry_id>",
+         AcceptanceLogView.as_view()),
 
     # todo fix this
-    path("board/<str:name>/<str:resource>", BoardView.as_view()),
+    path("board/<str:level_id>/<str:resource>", BoardView.as_view()),
 
     #
     # path("game/", GameView.as_view()),
