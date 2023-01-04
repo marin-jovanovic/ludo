529622fcb620db89d4da4141720df24217fe68ba
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-01-04 21:15:31 +0100

    backend api view level_view.py - wip: models cleanup

--- backend/api/view/level_view.py
+++ backend/api/view/level_view.py
@@ -1,10 +1,11 @@
-import urllib
-
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from backend.api.cqrs_c.level import create_game, leave_level, join_game, \
+from backend.api.cqrs_c.level import create_game, leave_level, join_level, \
     get_active_levels, in_which_level_is_user
+from backend.api.model.level import get_level_model
+from backend.api.model.player_order import get_player_order_model
+from backend.api.model.user import get_user_model
 from backend.api.view.comm import get_auth_ok_response_template
 
 
@@ -26,26 +27,22 @@ class LevelView(APIView):
 
         """
 
-    def get(self, request, name=None):
+    def get(self, request, level_id=None):
         # get basic game info
         response = get_auth_ok_response_template(request)
 
         # todo get in chunks (e.g. 10 by 10 with index and total number of pages in res)
         # todo add filters
 
-        if name:
+        if level_id:
             # raise NotImplementedError
 
             response["payload"]["status"] = True
 
-            from backend.api.model.player_order import get_player_order_model
-            from backend.api.model.player import get_user_model
-
-
-            r = get_player_order_model().objects.filter(level_id_id=name)
+            r = get_player_order_model().objects.filter(level_id=level_id)
 
             id_to_order = {
-                i.user_id : i.join_index for i in r
+                i.user_id: i.join_index for i in r
             }
 
             users_ids = [i.user_id for i in r]
@@ -58,19 +55,12 @@ class LevelView(APIView):
                 # todo add is active flag, when filtering in future
                 r = get_user_model().objects.get(id=id_)
 
-                # player_ord = get_player_order_model().objects.filter
-
                 m[id_] = {"username": r.username, "joinId": join_id}
 
             for i in m.items():
                 print(i)
 
-            # getleve
-
-            from backend.api.model.level import get_level_model
-
-            c = get_level_model().objects.get(id=name).capacity
-            capacity = -1
+            c = get_level_model().objects.get(id=level_id).capacity
             capacity = c
 
             response["payload"] = {
@@ -79,10 +69,6 @@ class LevelView(APIView):
                 "capacity": capacity
             }
 
-            # for i in r:
-            #     print(f"{i.user_id=}")
-
-            # response["payload"]["users"] =
 
         else:
 
@@ -90,7 +76,7 @@ class LevelView(APIView):
             if not r["status"]:
                 return JsonResponse(response)
 
-            response['payload']["levels"] =r["payload"]
+            response['payload']["levels"] = r["payload"]
 
             r = in_which_level_is_user(request.username)
             if not r["status"]:
@@ -104,8 +90,8 @@ class LevelView(APIView):
 
     # todo observers
 
-
-    def post(self, request, name):
+    # maybe level_id=None, level_name=None
+    def post(self, request, level_id):
         """
         create level
 
@@ -114,26 +100,27 @@ class LevelView(APIView):
 
         """
 
+        level_name = level_id
+
         capacity = request.data["capacity"]
 
         response = get_auth_ok_response_template(request)
-        response["payload"] = create_game(request.username, name, capacity)
+        response["payload"] = create_game(request.username, level_name,
+                                          capacity)
 
         return JsonResponse(response)
 
-    def put(self, request, name):
+    def put(self, request, level_id):
         """join or leave level"""
 
         username = request.username
 
         response = get_auth_ok_response_template(request)
 
-        print(f"{request.data=}")
-
         if "leave" in request.data:
-            response["payload"] = leave_level(name, username)
+            response["payload"] = leave_level(username)
 
         if "join" in request.data:
-            response["payload"] = join_game(name, username)
+            response["payload"] = join_level(level_id, username)
 
         return JsonResponse(response)
