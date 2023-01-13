6fee44da3284ed90af37d45c528f4162a82a8a03
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-01-13 02:10:33 +0100

    backend api view user_page_view.py - wip: ref

--- backend/api/view/user_page_view.py
+++ backend/api/view/user_page_view.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from backend.api.view.comm import get_auth_ok_response_template
 from backend.api.model.user import get_user_model
+from backend.api.view.comm import get_auth_ok_response_template
 
 
 class UserPageView(APIView):
@@ -18,33 +18,7 @@ class UserPageView(APIView):
 
         response = get_auth_ok_response_template(request)
 
-        # if user_id:
-        #
-        #     r = get_user_model().objects.get(id=user_id)
-        #
-        #     print(f"{r=}")
-        #
-        #     response["payload"] = {
-        #         "status": True,
-        #         "id": r.id,
-        #         "username": r.username,
-        #         "currentlyPlaying": r.currently_playing
-        #     }
-        #
-        #     return JsonResponse(response)
-        #
-        # else:
         print("user view")
-
-        # print(f"{request.data=}")
-        # print(f"{request.body=}")
-        # print(f"{request.headers=}")
-
-        # page = request.data["page"]
-
-
-
-        # print(f"{page=}")
 
         r = get_user_model().objects.all()
 
@@ -54,20 +28,11 @@ class UserPageView(APIView):
 
         # for e, i in enumerate(r):
         for i in r:
-
             users[i.id] = {
                 "userId": i.id,
                 "userUsername": i.username,
                 # "currentlyPlaying": r.currently_playing
             }
-
-        # users = {}
-        # for i in range(20):
-        #     users[i] = {
-        #         "userId": i * 26,
-        #         "userUsername": "johndoe" + str(i + 2),
-        #
-        #     }
 
         response["payload"] = {
             "status": True,
@@ -81,6 +46,4 @@ class UserPageView(APIView):
             }
         }
 
-
         return JsonResponse(response)
-
