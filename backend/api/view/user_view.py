46972c482f66a7070c58df8fd6d8d8a06a2cbbc0
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-01-13 02:10:32 +0100

    backend api view user_view.py - wip: ref

--- backend/api/view/user_view.py
+++ backend/api/view/user_view.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
-from backend.api.view.comm import get_auth_ok_response_template
 from backend.api.model.user import get_user_model
+from backend.api.view.comm import get_auth_ok_response_template
 
 
 class UserView(APIView):
@@ -39,37 +39,6 @@ class UserView(APIView):
             print(f"{request.data=}")
             print(f"{request.body=}")
             print(f"{request.headers=}")
-
-            # page = request.data["page"]
-            #
-            #
-            #
-            # print(f"{page=}")
-            #
-            # response["payload"] = {
-            #     "status": True,
-            #
-            #     "users": {
-            #         0: {
-            #             "userId": 1,
-            #             "userUsername": "johndoe",
-            #
-            #         },
-            #         1: {
-            #             "userId": 2,
-            #             "userUsername": "janedoe",
-            #         },
-            #     },
-            #     "meta": {
-            #         "totalUsers": 100,
-            #         "totalPages": 10,
-            #         "currentPage": 1,
-            #         "perPage": 10,
-            #         "thisPage": 2,
-            #
-            #     }
-            # }
-
 
             return JsonResponse(response)
 
