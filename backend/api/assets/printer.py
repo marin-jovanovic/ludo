8adcc7d1150bb24ba9efcf7f517a3316ff9d84e8
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-01-13 02:10:33 +0100

    backend api assets printer.py - wip: ref

--- backend/api/assets/printer.py
+++ backend/api/assets/printer.py
@@ -2,11 +2,14 @@ import pathlib
 
 
 def print_logo():
-    current_folder = pathlib.Path(__file__).parent.parent.parent.parent.resolve()
+    current_folder = pathlib.Path(
+        __file__).parent.parent.parent.parent.resolve()
 
     print(current_folder)
-    with open(current_folder / "resources" / "ascii_art" / "ascii_logo.txt", "r") as f:
+    with open(current_folder / "resources" / "ascii_art" / "ascii_logo.txt",
+              "r") as f:
         print(f.read())
+
 
 def main():
     print_logo()
